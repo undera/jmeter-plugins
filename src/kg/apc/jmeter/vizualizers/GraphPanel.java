f2c4bad0b53dee07ff4d08b61ce5bf60f8697e6d
apc@apc.kg <apc@apc.kg@173b9228-6733-11de-859f-df06e5afd5d7>
2010-01-31 18:11:07 +0000

    Started adding rows selector

--- src/kg/apc/jmeter/vizualizers/GraphPanel.java
+++ src/kg/apc/jmeter/vizualizers/GraphPanel.java
@@ -1,33 +1,83 @@
 // TODO: add slider to zoom Y axis
-
 package kg.apc.jmeter.vizualizers;
 
+import java.awt.Component;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import org.apache.jorphan.logging.LoggingManager;
-import org.apache.log.Logger;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import org.apache.jmeter.config.Argument;
+import org.apache.jmeter.gui.util.HeaderAsPropertyRenderer;
+import org.apache.jorphan.gui.ObjectTableModel;
+import org.apache.jorphan.reflect.Functor;
+import sun.swing.table.DefaultTableCellHeaderRenderer;
 
 public class GraphPanel
      extends JTabbedPane
 {
    private GraphPanelChart graphTab;
    private JComponent rowsTab;
+   private JTable table;
+   private ObjectTableModel tableModel;
 
    public GraphPanel()
    {
       super();
       addGraphTab();
-      //addRowsTab();
+      addRowsTab();
    }
 
    private void addRowsTab()
    {
       ImageIcon rowsIcon = createImageIcon("checks.png");
       rowsTab = new JPanel();
+      rowsTab.add(makeTable());
       addTab("Rows", rowsIcon, rowsTab, "Select rows to display");
+   }
+
+   private Component makeTable()
+   {
+      initializeTableModel();
+      table = new JTable(tableModel);
+      table.getTableHeader().setDefaultRenderer(new DefaultTableCellHeaderRenderer());
+      table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+      return makeScrollPane(table);
+   }
+
+   private JScrollPane makeScrollPane(Component comp)
+   {
+      JScrollPane pane = new JScrollPane(comp);
+      pane.setPreferredSize(pane.getMinimumSize());
+      return pane;
+   }
+
+   private void initializeTableModel()
+   {
+      tableModel = new ObjectTableModel(new String[]
+           {
+              "Row name",
+              "display?"
+           },
+           AbstractGraphRow.class,
+           new Functor[]
+           {
+              new Functor("getName"), // $NON-NLS-1$
+              new Functor("getValue")
+           }, // $NON-NLS-1$
+           new Functor[]
+           {
+              null, // $NON-NLS-1$
+              new Functor("setValue")
+           }, // $NON-NLS-1$
+           new Class[]
+           {
+              String.class,
+              String.class
+           });
    }
 
    private void addGraphTab()

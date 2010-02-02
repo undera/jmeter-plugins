f7ac81d2dcb66b83fde24802a19c61086b177354
apc@apc.kg <apc@apc.kg@173b9228-6733-11de-859f-df06e5afd5d7>
2010-02-02 10:39:12 +0000

    Legend colors added to rows selection

--- src/kg/apc/jmeter/vizualizers/ChartRowsTable.java
+++ src/kg/apc/jmeter/vizualizers/ChartRowsTable.java
@@ -3,12 +3,15 @@ package kg.apc.jmeter.vizualizers;
 import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.table.TableCellRenderer;
 import org.apache.jorphan.gui.ObjectTableModel;
 import org.apache.jorphan.reflect.Functor;
 
 public class ChartRowsTable
      extends JTable
 {
+   TableCellRenderer colorRenderer = new ColorRenderer(false);
+
    public ChartRowsTable()
    {
       super();
@@ -22,19 +25,23 @@ public class ChartRowsTable
    {
       ObjectTableModel model = new ObjectTableModel(new String[]
            {
-              "(un)check all",
-              "Row name"
+              "(Un)Check All",
+              "Legend Color",
+              "Row Name"
            }, AbstractGraphRow.class, new Functor[]
            {
               new Functor("isDrawOnChart"),
+              new Functor("getColor"),
               new Functor("getLabel")
            }, new Functor[]
            {
               new Functor("setDrawOnChart"),
+              null,
               null
            }, new Class[]
            {
               Boolean.class,
+              Color.class,
               String.class
            });
       setModel(model);
@@ -43,5 +50,15 @@ public class ChartRowsTable
    public void addRow(AbstractGraphRow row)
    {
       ((ObjectTableModel) dataModel).addRow(row);
+   }
+
+   @Override
+   public TableCellRenderer getCellRenderer(int row, int column)
+   {
+      if ((column == 1))
+      {
+         return colorRenderer;
+      }
+      return super.getCellRenderer(row, column);
    }
 }

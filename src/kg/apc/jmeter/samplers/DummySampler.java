0ca3faa3a25280a3867ed16bd38a893ba3190e18
apc@apc.kg <apc@apc.kg@173b9228-6733-11de-859f-df06e5afd5d7>
2010-02-10 13:11:34 +0000

    Dummy Sampler implementing

--- src/kg/apc/jmeter/samplers/DummySampler.java
+++ src/kg/apc/jmeter/samplers/DummySampler.java
@@ -3,47 +3,70 @@ package kg.apc.jmeter.samplers;
 import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 
 public class DummySampler
       extends AbstractSampler
 {
+   private static final Logger log = LoggingManager.getLoggerForClass();
+   public static String IS_SUCCESSFUL = "Successful";
+   public static String RESPONSE_CODE = "Response Code (e.g. 200)";
+   public static String RESPONSE_MESSAGE = "Response Message (e.g. OK)";
+   public static String RESPONSE_DATA = "Response Data";
+   private boolean successfull = true;
+   private String responseCode = "";
+   private String responseMessage = "";
+   private String responseData = "";
+
    public SampleResult sample(Entry e)
    {
+      log.info(responseData);
+
       SampleResult res = new SampleResult();
       res.setSampleLabel(getName());
+
+      // response time
+      res.sampleStart();
 
       // source data
       res.setSamplerData("");
 
       // response code
-      res.setResponseCode(getResponseCode());
-      res.setResponseMessage(getResponseMessage());
-      res.setSuccessful(isSuccessfull());
+      res.setResponseCode(responseCode);
+      res.setResponseMessage(responseMessage);
+      res.setSuccessful(successfull);
 
       // responde data
       res.setDataType(SampleResult.TEXT);
-      res.setResponseData(getResponseData().getBytes());
+      res.setResponseData(responseData.getBytes());
+
+      res.sampleEnd();
 
       return res;
    }
 
-   private String getResponseData()
+   public void setSuccessful(boolean selected)
    {
-      throw new UnsupportedOperationException("Not yet implemented");
+      setProperty(IS_SUCCESSFUL, selected);
+      successfull = selected;
    }
 
-   private String getResponseCode()
+   public void setResponseCode(String text)
    {
-      throw new UnsupportedOperationException("Not yet implemented");
+      setProperty(RESPONSE_CODE, text);
+      responseCode = text;
    }
 
-   private String getResponseMessage()
+   public void setResponseMessage(String text)
    {
-      throw new UnsupportedOperationException("Not yet implemented");
+      setProperty(RESPONSE_MESSAGE, text);
+      responseMessage = text;
    }
 
-   private boolean isSuccessfull()
+   public void setResponseData(String text)
    {
-      throw new UnsupportedOperationException("Not yet implemented");
+      setProperty(RESPONSE_DATA, text);
+      responseData = text;
    }
 }
--- src/kg/apc/jmeter/samplers/DummySamplerGui.java
+++ src/kg/apc/jmeter/samplers/DummySamplerGui.java
@@ -1,6 +1,9 @@
 package kg.apc.jmeter.samplers;
 
 import java.awt.BorderLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import org.apache.jmeter.gui.util.VerticalPanel;
 import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
 import org.apache.jmeter.testelement.TestElement;
@@ -8,58 +11,99 @@ import org.apache.jmeter.testelement.TestElement;
 public class DummySamplerGui
       extends AbstractSamplerGui
 {
-    private TCPConfigGui TcpDefaultPanel;
+   private JCheckBox isSuccessful;
+   private JTextField responseCode;
+   private JTextField responseMessage;
+   private JTextArea responseData;
 
-    public DummySamplerGui() {
-        init();
-    }
+   public DummySamplerGui()
+   {
+      init();
+   }
 
-    public void configure(TestElement element) {
-        super.configure(element);
-        TcpDefaultPanel.configure(element);
-    }
+   @Override
+   public String getStaticLabel()
+   {
+      return "Dummy Sampler";
+   }
 
-    public TestElement createTestElement() {
-        DummySampler sampler = new DummySampler();
-        modifyTestElement(sampler);
-        return sampler;
-    }
+   @Override
+   public void configure(TestElement element)
+   {
+      super.configure(element);
 
-    /**
-     * Modifies a given TestElement to mirror the data in the gui components.
-     *
-     * @see org.apache.jmeter.gui.JMeterGUIComponent#modifyTestElement(TestElement)
-     */
-    public void modifyTestElement(TestElement sampler) {
-        sampler.clear();
-        sampler.addTestElement(TcpDefaultPanel.createTestElement());
-        this.configureTestElement(sampler);
-    }
+      isSuccessful.setSelected(element.getPropertyAsBoolean(DummySampler.IS_SUCCESSFUL));
+      responseCode.setText(element.getPropertyAsString(DummySampler.RESPONSE_CODE));
+      responseMessage.setText(element.getPropertyAsString(DummySampler.RESPONSE_MESSAGE));
+      responseData.setText(element.getPropertyAsString(DummySampler.RESPONSE_DATA));
+   }
 
-    /**
-     * Implements JMeterGUIComponent.clearGui
-     */
-    public void clearGui() {
-        super.clearGui();
+   public TestElement createTestElement()
+   {
+      DummySampler sampler = new DummySampler();
+      modifyTestElement(sampler);
+      return sampler;
+   }
 
-        TcpDefaultPanel.clearGui();
-    }
+   /**
+    * Modifies a given TestElement to mirror the data in the gui components.
+    *
+    * @see org.apache.jmeter.gui.JMeterGUIComponent#modifyTestElement(TestElement)
+    */
+   public void modifyTestElement(TestElement sampler)
+   {
+      super.configureTestElement(sampler);
 
-    public String getLabelResource() {
-        return "tcp_sample_title"; // $NON-NLS-1$
-    }
+      if (sampler instanceof DummySampler)
+      {
+         DummySampler dummySampler = (DummySampler) sampler;
+         dummySampler.setSuccessful(isSuccessful.isSelected());
+         dummySampler.setResponseCode(responseCode.getText());
+         dummySampler.setResponseMessage(responseMessage.getText());
+         dummySampler.setResponseData(responseData.getText());
+      }
+   }
 
-    private void init() {
-        setLayout(new BorderLayout(0, 5));
-        setBorder(makeBorder());
+   /**
+    * Implements JMeterGUIComponent.clearGui
+    */
+   @Override
+   public void clearGui()
+   {
+      super.clearGui();
 
-        add(makeTitlePanel(), BorderLayout.NORTH);
+      isSuccessful.setSelected(true);
+      responseCode.setText("200");
+      responseMessage.setText("OK");
+      responseData.setText("");
+   }
 
-        VerticalPanel mainPanel = new VerticalPanel();
+   public String getLabelResource()
+   {
+      return this.getClass().getSimpleName();
+   }
 
-        TcpDefaultPanel = new TCPConfigGui(false);
-        mainPanel.add(TcpDefaultPanel);
+   private void init()
+   {
+      setLayout(new BorderLayout(0, 5));
+      setBorder(makeBorder());
 
-        add(mainPanel, BorderLayout.CENTER);
-    }
+      add(makeTitlePanel(), BorderLayout.NORTH);
+
+      VerticalPanel mainPanel = new VerticalPanel();
+
+      isSuccessful=new JCheckBox(DummySampler.IS_SUCCESSFUL);
+      mainPanel.add(isSuccessful);
+
+      responseCode=new JTextField();
+      mainPanel.add(responseCode);
+
+      responseMessage=new JTextField();
+      mainPanel.add(responseMessage);
+
+      responseData=new JTextArea();
+      mainPanel.add(responseData);
+
+      add(mainPanel, BorderLayout.CENTER);
+   }
 }

874e0dc8406315c51dbda617bb8f9ba4f4fabffe
apc@apc.kg <apc@apc.kg@173b9228-6733-11de-859f-df06e5afd5d7>
2010-05-19 08:56:21 +0000

    Additional rampup introduced

--- src/kg/apc/jmeter/threads/SteppingThreadGroup.java
+++ src/kg/apc/jmeter/threads/SteppingThreadGroup.java
@@ -1,31 +1,34 @@
+// TODO: сделать плагин с гридовым указанием юзеров
 package kg.apc.jmeter.threads;
 
 import org.apache.jmeter.threads.AbstractThreadGroup;
 import org.apache.jmeter.threads.JMeterThread;
+import org.apache.jmeter.util.JMeterUtils;
 
 public class SteppingThreadGroup
-     extends AbstractThreadGroup
+      extends AbstractThreadGroup
 {
-   public static final String THREAD_GROUP_DELAY="Threads initial delay";
-   public static final String INC_USER_PERIOD="Start users period";
-   public static final String INC_USER_COUNT="Start users count";
-   public static final String DEC_USER_PERIOD="Stop users period";
-   public static final String DEC_USER_COUNT="Stop users count";
-   public static final String FLIGHT_TIME="flighttime";
+   public static final String THREAD_GROUP_DELAY = "Threads initial delay";
+   public static final String INC_USER_PERIOD = "Start users period";
+   public static final String INC_USER_COUNT = "Start users count";
+   public static final String DEC_USER_PERIOD = "Stop users period";
+   public static final String DEC_USER_COUNT = "Stop users count";
+   public static final String FLIGHT_TIME = "flighttime";
 
    public SteppingThreadGroup()
    {
       super();
    }
 
-   /**
-    * This will schedule the time for the JMeterThread.
-    *
-    * @param thread
-    * @param group
-    */
    public void scheduleThread(JMeterThread thread)
    {
+      int additionalRampUp = 0;
+      String rampupProperty = JMeterUtils.getProperty("steppingthreadgroup.additionalrampup");
+      if (rampupProperty != null)
+      {
+         additionalRampUp = Integer.parseInt(rampupProperty);
+      }
+
       int threadGroupDelay = getThreadGroupDelay();
 
       int inUserPeriod = getInUserPeriod();
@@ -39,7 +42,7 @@ public class SteppingThreadGroup
       long ascentPoint = System.currentTimeMillis() + 1000 * threadGroupDelay;
       long descentPoint = ascentPoint + 1000 * inUserPeriod * (int) Math.floor((double) getNumThreads() / inUserCount) + 1000 * flightTime;
 
-      thread.setStartTime(ascentPoint + 1000 * inUserPeriod * (int) Math.floor((double) thread.getThreadNum() / inUserCount));
+      thread.setStartTime(ascentPoint + 1000 * inUserPeriod * (int) Math.floor((double) thread.getThreadNum() / inUserCount) + additionalRampUp * thread.getThreadNum());
       thread.setEndTime(descentPoint + 1000 * outUserPeriod * (int) Math.floor((double) thread.getThreadNum() / outUserCount));
       thread.setScheduled(true);
    }

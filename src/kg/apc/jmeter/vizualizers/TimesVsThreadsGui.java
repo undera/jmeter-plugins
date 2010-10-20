4ba919ba7f63d991aa07fc454563dfccb1fed42b
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2010-10-20 14:17:50 +0000

    Fix non thread safe rows creation

--- src/kg/apc/jmeter/vizualizers/TimesVsThreadsGui.java
+++ src/kg/apc/jmeter/vizualizers/TimesVsThreadsGui.java
@@ -11,76 +11,88 @@ import org.apache.jmeter.samplers.SampleResult;
  * @author apc
  */
 public class TimesVsThreadsGui
-     extends AbstractGraphPanelVisualizer
+        extends AbstractGraphPanelVisualizer
 {
-   /**
-    *
-    */
-   public TimesVsThreadsGui()
-   {
-      super();
-      graphPanel.getGraphObject().setDrawCurrentX(true);
-      graphPanel.getGraphObject().setForcedMinX(0);
-   }
 
-   public String getLabelResource()
-   {
-      return this.getClass().getSimpleName();
-   }
+    /**
+     *
+     */
+    public TimesVsThreadsGui()
+    {
+        super();
+        graphPanel.getGraphObject().setDrawCurrentX(true);
+        graphPanel.getGraphObject().setForcedMinX(0);
+    }
 
-   @Override
-   public String getStaticLabel()
-   {
-      return "Response Times vs Threads";
-   }
+    public String getLabelResource()
+    {
+        return this.getClass().getSimpleName();
+    }
 
-   public void add(SampleResult res)
-   {
-      String label = res.getSampleLabel();
-      String averageLabel = "Average " + res.getSampleLabel();
-      GraphRowAverages row;
-      GraphRowOverallAverages avgRow;
-      if (!model.containsKey(label))
-      {
-         final Color nextColor = colors.getNextColor();
-         row = getNewRow(label, nextColor);
-         avgRow = getNewAveragesRow(averageLabel, nextColor);
-         graphPanel.addRow(row);
-         graphPanel.addRow(avgRow);
-      }
-      else
-      {
-         row = (GraphRowAverages) model.get(label);
-         avgRow = (GraphRowOverallAverages) model.get(averageLabel);
-      }
+    @Override
+    public String getStaticLabel()
+    {
+        return "Response Times vs Threads";
+    }
 
-      row.add(res.getAllThreads(), res.getTime());
-      avgRow.add(res.getAllThreads(), res.getTime());
+    public void add(SampleResult res)
+    {
+        String label = res.getSampleLabel();
+        String averageLabel = "Average " + res.getSampleLabel();
+        GraphRowAverages row;
+        GraphRowOverallAverages avgRow;
+        if (!model.containsKey(label))
+        {
+            row = getNewRow(label);
+            avgRow = getNewAveragesRow(averageLabel, row.getColor());
+        } else
+        {
+            row = (GraphRowAverages) model.get(label);
+            avgRow = (GraphRowOverallAverages) model.get(averageLabel);
+        }
 
-      graphPanel.getGraphObject().setCurrentX(res.getAllThreads());
-      updateGui(null);
-   }
+        row.add(res.getAllThreads(), res.getTime());
+        avgRow.add(res.getAllThreads(), res.getTime());
 
-   private GraphRowOverallAverages getNewAveragesRow(String averageLabel, final Color nextColor)
-   {
-      GraphRowOverallAverages avgRow = new GraphRowOverallAverages();
-      avgRow.setLabel(averageLabel);
-      avgRow.setColor(nextColor);
-      avgRow.setMarkerSize(AbstractGraphRow.MARKER_SIZE_BIG);
-      avgRow.setDrawValueLabel(true);
-      avgRow.setShowInLegend(false);
-      model.put(averageLabel, avgRow);
-      return avgRow;
-   }
+        graphPanel.getGraphObject().setCurrentX(res.getAllThreads());
+        updateGui(null);
+    }
 
-   private GraphRowAverages getNewRow(String label, final Color nextColor)
-   {
-      GraphRowAverages row = new GraphRowAverages();
-      row.setLabel(label);
-      row.setColor(nextColor);
-      row.setDrawLine(true);
-      row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
-      model.put(label, row);
-      return row;
-   }
+    private synchronized GraphRowOverallAverages getNewAveragesRow(String averageLabel, Color color)
+    {
+        GraphRowOverallAverages avgRow = null;
+        if (!model.containsKey(averageLabel))
+        {
+            avgRow = new GraphRowOverallAverages();
+            avgRow.setLabel(averageLabel);
+            avgRow.setColor(color);
+            avgRow.setMarkerSize(AbstractGraphRow.MARKER_SIZE_BIG);
+            avgRow.setDrawValueLabel(true);
+            avgRow.setShowInLegend(false);
+            model.put(averageLabel, avgRow);
+            graphPanel.addRow(avgRow);
+        } else {
+            avgRow = (GraphRowOverallAverages) model.get(averageLabel);
+        }
+        return avgRow;
+    }
+
+    private synchronized GraphRowAverages getNewRow(String label)
+    {
+        GraphRowAverages row = null;
+        if (!model.containsKey(label))
+        {
+            row = new GraphRowAverages();
+            row.setLabel(label);
+            row.setColor(colors.getNextColor());
+            row.setDrawLine(true);
+            row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
+            model.put(label, row);
+            graphPanel.addRow(row);
+        } else
+        {
+            row = (GraphRowAverages) model.get(label);
+        }
+        return row;
+    }
 }
--- src/kg/apc/jmeter/vizualizers/TotalTransactionsPerSecondGui.java
+++ src/kg/apc/jmeter/vizualizers/TotalTransactionsPerSecondGui.java
@@ -9,59 +9,73 @@ import org.apache.jmeter.samplers.SampleResult;
  * @author Stephane Hoblingre
  */
 public class TotalTransactionsPerSecondGui
-      extends AbstractGraphPanelVisualizer
+        extends AbstractGraphPanelVisualizer
 {
-   //private static final Logger log = LoggingManager.getLoggerForClass();
-   /**
-    *
-    */
+    //private static final Logger log = LoggingManager.getLoggerForClass();
 
-   public TotalTransactionsPerSecondGui()
-   {
-      super();
-      graphPanel.getGraphObject().setxAxisLabelRenderer(new DateTimeRenderer(
-            "HH:mm:ss"));
-      graphPanel.getGraphObject().setDrawFinalZeroingLines(false);
-      delay = 1000;
-   }
+    /**
+     *
+     */
+    public TotalTransactionsPerSecondGui()
+    {
+        super();
+        graphPanel.getGraphObject().setxAxisLabelRenderer(new DateTimeRenderer(
+                "HH:mm:ss"));
+        graphPanel.getGraphObject().setDrawFinalZeroingLines(false);
+        delay = 1000;
+    }
 
-   private void addTransaction(String threadGroupName, long time, int count)
-   {
-      AbstractGraphRow row = model.get(threadGroupName);
-      if (row == null)
-      {
-         row = new GraphRowSumValues(false);
-         row.setLabel(threadGroupName);
-         row.setColor(colors.getNextColor());
-         row.setDrawLine(true);
-         row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
-         model.put(threadGroupName, row);
-         graphPanel.addRow(row);
-      }
+    private synchronized AbstractGraphRow getNewRow(String label)
+    {
+        AbstractGraphRow row = null;
+        if(!model.containsKey(label)) {
+            row = new GraphRowSumValues(false);
+            row.setLabel(label);
+            row.setColor(colors.getNextColor());
+            row.setDrawLine(true);
+            row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
+            model.put(label, row);
+            graphPanel.addRow(row);
+        } else {
+            row = model.get(label);
+        }
 
-      row.add(time, count);
-   }
+        return row;
+    }
 
-   public String getLabelResource()
-   {
-      return this.getClass().getSimpleName();
-   }
+    private void addTransaction(String threadGroupName, long time, int count)
+    {
+        AbstractGraphRow row = model.get(threadGroupName);
+        if (row == null)
+        {
+            row = getNewRow(threadGroupName);
+        }
 
-   @Override
-   public String getStaticLabel()
-   {
-      return "Total Transactions Per Second";
-   }
+        row.add(time, count);
+    }
 
-   public void add(SampleResult res)
-   {
-       //always add 0 failed transactions
-       if(res.isSuccessful()) {
-        addTransaction("Total Failed Transactions", res.getEndTime() - res.getEndTime() % delay, 0);
-        addTransaction("Total Successful Transactions", res.getEndTime() - res.getEndTime() % delay, 1);
-       } else {
-        addTransaction("Total Failed Transactions", res.getEndTime() - res.getEndTime() % delay, 1);
-       }
-      updateGui(null);
-   }
+    public String getLabelResource()
+    {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public String getStaticLabel()
+    {
+        return "Total Transactions Per Second";
+    }
+
+    public void add(SampleResult res)
+    {
+        //always add 0 failed transactions
+        if (res.isSuccessful())
+        {
+            addTransaction("Total Failed Transactions", res.getEndTime() - res.getEndTime() % delay, 0);
+            addTransaction("Total Successful Transactions", res.getEndTime() - res.getEndTime() % delay, 1);
+        } else
+        {
+            addTransaction("Total Failed Transactions", res.getEndTime() - res.getEndTime() % delay, 1);
+        }
+        updateGui(null);
+    }
 }

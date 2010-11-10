fd0b804680d9352ebd13f80ca3b8d978cdee8413
Stephane.Hoblingre <Stephane.Hoblingre@173b9228-6733-11de-859f-df06e5afd5d7>
2010-11-10 18:09:06 +0000

    Naming convention

---
+++ test/kg/apc/jmeter/vizualizers/ResponseTimesPercentilesGuiTest.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kg.apc.jmeter.vizualizers;
+
+import kg.apc.jmeter.util.TestJMeterUtils;
+import org.apache.jmeter.samplers.SampleResult;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Stéphane Hoblingre
+ */
+public class ResponseTimesPercentilesGuiTest {
+
+    public ResponseTimesPercentilesGuiTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+        TestJMeterUtils.createJmeterEnv();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getLabelResource method, of class ResponseTimesPercentilesGUI.
+     */
+    @Test
+    public void testGetLabelResource()
+    {
+        System.out.println("getLabelResource");
+        ResponseTimesPercentilesGui instance = new ResponseTimesPercentilesGui();
+        String expResult = "ResponseTimesPercentilesGui";
+        String result = instance.getLabelResource();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getStaticLabel method, of class ResponseTimesPercentilesGUI.
+     */
+    @Test
+    public void testGetStaticLabel()
+    {
+        System.out.println("getStaticLabel");
+        ResponseTimesPercentilesGui instance = new ResponseTimesPercentilesGui();
+        String expResult = "Response Times Percentiles";
+        String result = instance.getStaticLabel();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of add method, of class ResponseTimesPercentilesGUI.
+     */
+    @Test
+    public void testAdd()
+    {
+        System.out.println("add");
+        SampleResult res = new SampleResult();
+        res.setAllThreads(1);
+        res.setThreadName("test 1-2");
+        ResponseTimesPercentilesGui instance = new ResponseTimesPercentilesGui();
+        instance.add(res);
+    }
+
+    /**
+     * Test of getSettingsPanel method, of class ResponseTimesPercentilesGUI.
+     */
+    @Test
+    public void testGetSettingsPanel()
+    {
+        System.out.println("getSettingsPanel");
+        ResponseTimesPercentilesGui instance = new ResponseTimesPercentilesGui();
+        JSettingsPanel result = instance.getSettingsPanel();
+        assertNotNull(result);
+    }
+
+}

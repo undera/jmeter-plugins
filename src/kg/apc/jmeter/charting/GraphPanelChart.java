fb1192e2b8bea0bc6174362628fcc1c3d186d960
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2010-11-11 13:30:37 +0000

    Perfect spacing between bars (always 1 pixel)

--- src/kg/apc/jmeter/charting/GraphPanelChart.java
+++ src/kg/apc/jmeter/charting/GraphPanelChart.java
@@ -802,12 +802,13 @@ public class GraphPanelChart
           {
               g.setColor(row.getColor());
               if (isChartPointValid(x, y))
-              {
-                  g.drawRect(x, y, (int) (dxForDVal * row.getGranulationValue()) - 2, yHeight);
+              {   //x = chartRect.x + (int) ((calcPointX - minXVal) * dxForDVal);
+                  int x2 = chartRect.x + (int) ((calcPointX + row.getGranulationValue() - minXVal) * dxForDVal) - x -2;
+                  g.drawRect(x, y, x2, yHeight);
                   Composite oldComposite = ((Graphics2D) g).getComposite();
                   ((Graphics2D) g).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
 
-                  g.fillRect(x, y, (int) (dxForDVal * row.getGranulationValue()) - 2, yHeight);
+                  g.fillRect(x, y, x2 , yHeight);
                   ((Graphics2D) g).setComposite(oldComposite);
               }
           }

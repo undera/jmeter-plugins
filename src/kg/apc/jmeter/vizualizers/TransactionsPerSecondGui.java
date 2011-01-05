bb55d8cdaf4a100fa5013e05e28366b2b8c8f6cb
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-01-05 13:33:20 +0000

    Implement aggregate for Tans per Sec, add detailed trans failures rows

--- src/kg/apc/jmeter/vizualizers/TransactionsPerSecondGui.java
+++ src/kg/apc/jmeter/vizualizers/TransactionsPerSecondGui.java
@@ -1,5 +1,7 @@
 package kg.apc.jmeter.vizualizers;
 
+import java.awt.Color;
+import java.util.concurrent.ConcurrentSkipListMap;
 import kg.apc.jmeter.charting.AbstractGraphRow;
 import kg.apc.jmeter.charting.GraphRowSumValues;
 import org.apache.jmeter.samplers.SampleResult;
@@ -9,77 +11,122 @@ import org.apache.jmeter.samplers.SampleResult;
  * @author Stephane hoblingre
  */
 public class TransactionsPerSecondGui
-      extends AbstractGraphPanelVisualizer
+        extends AbstractGraphPanelVisualizer
 {
-   //private static final Logger log = LoggingManager.getLoggerForClass();
-   /**
-    *
-    */
-   public TransactionsPerSecondGui()
-   {
-      super();
-      graphPanel.getGraphObject().setxAxisLabelRenderer(new DateTimeRenderer(
-            "HH:mm:ss"));
-      graphPanel.getGraphObject().setDrawFinalZeroingLines(true);
-      setGranulation(1000);
-   }
+    //private static final Logger log = LoggingManager.getLoggerForClass();
 
-   private synchronized AbstractGraphRow getNewRow(String label)
-   {
-      AbstractGraphRow row = null;
-      if (!model.containsKey(label))
-      {
-         row = new GraphRowSumValues(false);
-         row.setLabel(label);
-         row.setColor(colors.getNextColor());
-         row.setDrawLine(true);
-         row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
-         model.put(label, row);
-         graphPanel.addRow(row);
-      }
-      else
-      {
-         row = model.get(label);
-      }
+    /**
+     *
+     */
+    private static String labelAggSuccess = "Successful Transactions per Second";
+    private static String labelAggFailure = "Failed Transactions per Second";
 
-      return row;
-   }
-
-    private void addOneTransaction(String threadGroupName, long time)
+    public TransactionsPerSecondGui()
     {
-        AbstractGraphRow row = model.get(threadGroupName);
+        super();
+        graphPanel.getGraphObject().setxAxisLabelRenderer(new DateTimeRenderer(
+                "HH:mm:ss"));
+        graphPanel.getGraphObject().setDrawFinalZeroingLines(true);
+        setGranulation(1000);
+    }
+
+    private synchronized AbstractGraphRow getNewRow(ConcurrentSkipListMap<String, AbstractGraphRow> model, String label, Color forcedColor)
+    {
+        AbstractGraphRow row = null;
+        if (!model.containsKey(label))
+        {
+            row = new GraphRowSumValues(false);
+            row.setLabel(label);
+            if(forcedColor == null)
+            {
+                row.setColor(colors.getNextColor());
+            } else
+            {
+                row.setColor(forcedColor);
+            }
+            row.setDrawLine(true);
+            row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
+            model.put(label, row);
+            graphPanel.addRow(row);
+        } else
+        {
+            row = model.get(label);
+        }
+
+        return row;
+    }
+
+    private AbstractGraphRow getNewRow(ConcurrentSkipListMap<String, AbstractGraphRow> model, String label)
+    {
+        return getNewRow(model, label, null);
+    }
+
+    private void addTransaction(boolean isSuccess, String rowName, long time, double count)
+    {
+        String realRowName = null;
+        String rowAggName = null;
+
+        if(isSuccess)
+        {
+            realRowName = rowName + " (success)";
+            rowAggName = labelAggSuccess;
+        } else
+        {
+            realRowName = rowName + " (failure)";
+            rowAggName = labelAggFailure;
+        }
+
+        AbstractGraphRow row = model.get(realRowName);
+        AbstractGraphRow rowAgg = modelAggregate.get(rowAggName);
+
         if (row == null)
         {
-            row = getNewRow(threadGroupName);
+            row = getNewRow(model, realRowName);
+        }
+        if (rowAgg == null)
+        {
+            rowAgg = getNewRow(modelAggregate, rowAggName, isSuccess ? Color.green : Color.red);
         }
 
         //fix to have trans/sec values in all cases
         if (getGranulation() > 0)
         {
-            row.add(time, 1000.0d / getGranulation());
+            row.add(time, count * 1000.0d / getGranulation());
+            rowAgg.add(time, count * 1000.0d / getGranulation());
         }
     }
 
-   public String getLabelResource()
-   {
-      return this.getClass().getSimpleName();
-   }
+    @Override
+    public String getLabelResource()
+    {
+        return this.getClass().getSimpleName();
+    }
 
-   @Override
-   public String getStaticLabel()
-   {
-      return "Transactions per Second";
-   }
+    @Override
+    public String getStaticLabel()
+    {
+        return "Transactions per Second";
+    }
 
-   public void add(SampleResult res)
-   {
-      addOneTransaction(res.getSampleLabel(), res.getEndTime() - res.getEndTime() % getGranulation());
-      updateGui(null);
-   }
+    @Override
+    public void add(SampleResult res)
+    {
+        //always add 0 failed transactions
+        if (res.isSuccessful())
+        {
+            addTransaction(false, res.getSampleLabel(), res.getEndTime() - res.getEndTime() % getGranulation(), 0);
+            addTransaction(true, res.getSampleLabel(), res.getEndTime() - res.getEndTime() % getGranulation(), 1);
+
+        } else
+        {
+            addTransaction(false, res.getSampleLabel(), res.getEndTime() - res.getEndTime() % getGranulation(), 1);
+        }
+        updateGui(null);
+    }
 
     @Override
     protected JSettingsPanel getSettingsPanel()
     {
-        return new JSettingsPanel(this, true, true, false, true, true);
+        return new JSettingsPanel(this, true, true, false, true, true, false, false, true);
     }
 }

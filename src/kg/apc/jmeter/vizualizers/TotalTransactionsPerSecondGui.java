aac5d32c3a8bc294ff6bab6966573a3d47d482eb
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-01-07 12:25:10 +0000

    Set listener as obsolete

--- src/kg/apc/jmeter/vizualizers/TotalTransactionsPerSecondGui.java
+++ src/kg/apc/jmeter/vizualizers/TotalTransactionsPerSecondGui.java
@@ -1,7 +1,5 @@
 package kg.apc.jmeter.vizualizers;
 
-import kg.apc.jmeter.charting.AbstractGraphRow;
-import kg.apc.jmeter.charting.GraphRowSumValues;
 import org.apache.jmeter.samplers.SampleResult;
 
 /**
@@ -11,7 +9,7 @@ import org.apache.jmeter.samplers.SampleResult;
 public class TotalTransactionsPerSecondGui
         extends AbstractGraphPanelVisualizer
 {
-    //private static final Logger log = LoggingManager.getLoggerForClass();
+    private static String obsoleteMsg = "This chart is obsolete. Please replace it with the standard \"Transactions per Seconds\" one.";
 
     /**
      *
@@ -19,43 +17,7 @@ public class TotalTransactionsPerSecondGui
     public TotalTransactionsPerSecondGui()
     {
         super();
-        graphPanel.getGraphObject().setxAxisLabelRenderer(new DateTimeRenderer(
-                "HH:mm:ss"));
-        graphPanel.getGraphObject().setDrawFinalZeroingLines(true);
-        setGranulation(1000);
-    }
-
-    private synchronized AbstractGraphRow getNewRow(String label)
-    {
-        AbstractGraphRow row = null;
-        if(!model.containsKey(label)) {
-            row = new GraphRowSumValues(false);
-            row.setLabel(label);
-            row.setColor(colors.getNextColor());
-            row.setDrawLine(true);
-            row.setMarkerSize(AbstractGraphRow.MARKER_SIZE_SMALL);
-            model.put(label, row);
-            graphPanel.addRow(row);
-        } else {
-            row = model.get(label);
-        }
-
-        return row;
-    }
-
-    private void addTransaction(String threadGroupName, long time, double count)
-    {
-        AbstractGraphRow row = model.get(threadGroupName);
-        if (row == null)
-        {
-            row = getNewRow(threadGroupName);
-        }
-
-        //fix to have trans/sec values in all cases
-        if (getGranulation() > 0)
-        {
-            row.add(time, count * 1000.0d / getGranulation());
-        }
+        graphPanel.getGraphObject().setErrorMessage(obsoleteMsg);
     }
 
     public String getLabelResource()
@@ -66,26 +28,18 @@ public class TotalTransactionsPerSecondGui
     @Override
     public String getStaticLabel()
     {
-        return "Total Transactions Per Second";
+        return "Total Transactions Per Second (obsolete)";
     }
 
     public void add(SampleResult res)
     {
-        //always add 0 failed transactions
-        if (res.isSuccessful())
-        {
-            addTransaction("Failed Transactions per Second", res.getEndTime() - res.getEndTime() % getGranulation(), 0);
-            addTransaction("Successful Transactions per Second", res.getEndTime() - res.getEndTime() % getGranulation(), 1);
-        } else
-        {
-            addTransaction("Failed Transactions per Second", res.getEndTime() - res.getEndTime() % getGranulation(), 1);
-        }
+        graphPanel.getGraphObject().setErrorMessage(obsoleteMsg);
         updateGui(null);
     }
 
     @Override
     protected JSettingsPanel getSettingsPanel()
     {
-        return new JSettingsPanel(this, true, true, false, true, true);
+        return new JSettingsPanel(this, false, true, false, false, false);
     }
 }

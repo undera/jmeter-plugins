1800efe6f198e671a36388fe0b469004b3a411c9
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-02-26 02:47:13 +0000

    Added option to remove plugin prefix Proposal to use "jp@gc - " as default prefix

--- src/kg/apc/jmeter/JMeterPluginsUtils.java
+++ src/kg/apc/jmeter/JMeterPluginsUtils.java
@@ -1,14 +1,20 @@
 package kg.apc.jmeter;
 
+import org.apache.jmeter.util.JMeterUtils;
+
 /**
  *
  * @author undera
  */
-public class JMeterPluginsUtils {
+public class JMeterPluginsUtils
+{
+
+    private static String pluginsPrefix = "jp@gc - ";
 
     // just prefix all the labels to be distinguished
-    public static String prefixLabel(String string) {
-        return "JPGC - "+string;
+    public static String prefixLabel(String label)
+    {
+        return pluginsPrefix + label;
     }
 
     public static String getStackTrace(Exception ex) {
@@ -21,4 +27,12 @@ public class JMeterPluginsUtils {
         return res;
     }
 
+    static
+    {
+        String pluginsPrefix = JMeterUtils.getProperty("jmeterPlugin.pluginsPrefix");
+        if (pluginsPrefix != null)
+        {
+            JMeterPluginsUtils.pluginsPrefix = pluginsPrefix;
+        }
+    }
 }

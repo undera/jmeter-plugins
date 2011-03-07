18d0c14b0f0f13c15578d4d48e72b1fb440c917a
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-03-07 13:08:00 +0000

    Unit tests for add distributed tests handling for vs Thread listeners

---
+++ test/kg/apc/jmeter/vizualizers/AbstractVsThreadVisualizerTest.java
@@ -0,0 +1,108 @@
+package kg.apc.jmeter.vizualizers;
+
+import kg.apc.jmeter.util.TestJMeterUtils;
+import org.apache.jmeter.samplers.SampleResult;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Stephane Hoblingre
+ */
+public class AbstractVsThreadVisualizerTest {
+
+    public AbstractVsThreadVisualizerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+        TestJMeterUtils.createJmeterEnv();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCurrentThreadCount method, of class AbstractVsThreadVisualizer.
+     */
+    @Test
+    public void testGetCurrentThreadCount()
+    {
+        System.out.println("getCurrentThreadCount");
+        long now = System.currentTimeMillis();
+        SampleResult sample = new SampleResult();
+        sample.setAllThreads(3);
+        sample.setThreadName("test_tg");
+        sample.setStampAndTime(now, 300);
+        AbstractVsThreadVisualizer instance = new AbstractVsThreadVisualizerImpl();
+        instance.add(sample);
+        int expResult = 3;
+        int result = instance.getCurrentThreadCount(sample);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of add method, of class AbstractVsThreadVisualizer.
+     */
+    @Test
+    public void testAdd()
+    {
+        System.out.println("add");
+        SampleResult sample = new SampleResult();
+        AbstractVsThreadVisualizer instance = new AbstractVsThreadVisualizerImpl();
+    }
+
+    /**
+     * Test of clearData method, of class AbstractVsThreadVisualizer.
+     */
+    @Test
+    public void testClearData()
+    {
+        System.out.println("clearData");
+        AbstractVsThreadVisualizer instance = new AbstractVsThreadVisualizerImpl();
+        SampleResult sample = new SampleResult();
+        instance.clearData();
+    }
+
+    public class AbstractVsThreadVisualizerImpl extends AbstractVsThreadVisualizer
+    {
+        @Override
+        protected JSettingsPanel getSettingsPanel()
+        {
+            return new JSettingsPanel(this, isStats, isStats, isStats, isStats, isStats);
+        }
+
+        @Override
+        public String getLabelResource()
+        {
+            return "";
+        }
+
+        @Override
+        public String getStaticLabel()
+        {
+            return "TEST";
+        }
+
+        @Override
+        protected String getWikiPage() {
+           return "";
+        }
+    }
+
+}

713c8abb1116e1a26c7e5144e56c5870b4a9026d
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-08-05 09:18:51 +0000

    add small description in GUI

--- src/kg/apc/jmeter/reporters/ConsoleStatusLoggerGui.java
+++ src/kg/apc/jmeter/reporters/ConsoleStatusLoggerGui.java
@@ -1,6 +1,8 @@
 package kg.apc.jmeter.reporters;
 
 import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import kg.apc.jmeter.JMeterPluginsUtils;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.visualizers.gui.AbstractListenerGui;
@@ -23,10 +25,12 @@ public class ConsoleStatusLoggerGui extends AbstractListenerGui {
         return JMeterPluginsUtils.prefixLabel("Console Status Logger");
     }
 
+   @Override
     public String getLabelResource() {
         return getClass().getCanonicalName();
     }
 
+   @Override
     public TestElement createTestElement() {
         TestElement te = new ConsoleStatusLogger();
         modifyTestElement(te);
@@ -34,6 +38,7 @@ public class ConsoleStatusLoggerGui extends AbstractListenerGui {
         return te;
     }
 
+   @Override
     public void modifyTestElement(TestElement te) {
         super.configureTestElement(te);
     }
@@ -42,5 +47,21 @@ public class ConsoleStatusLoggerGui extends AbstractListenerGui {
         setLayout(new BorderLayout(0, 5));
         setBorder(makeBorder());
         add(JMeterPluginsUtils.addHelpLinkToPanel(makeTitlePanel(), WIKIPAGE), BorderLayout.NORTH);
+        JTextArea info = new JTextArea();
+        info.setEditable(false);
+        info.setWrapStyleWord(true);
+        info.setOpaque(false);
+        info.setLineWrap(true);
+        info.setColumns(20);
+
+        JScrollPane jScrollPane1 = new javax.swing.JScrollPane();
+        jScrollPane1.setViewportView(info);
+        jScrollPane1.setBorder(null);
+        
+        info.setText("This is a simple listener that prints short summary log to console while JMeter is running in non-GUI mode. "
+                + "It also writes the same info into jmeter.log in GUI mode."
+                + "\n\nNote that response time and latency values printed are averages.");
+
+        add(jScrollPane1, BorderLayout.CENTER);
     }
 }

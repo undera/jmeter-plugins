b93c648251786ee235bffd55e1b3dd4c5672ebe4
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-10-31 09:24:28 +0000

    Bar Plotter

---
+++ plugins/src/kg/apc/charting/plotters/BarRowPlotter.java
@@ -0,0 +1,38 @@
+package kg.apc.charting.plotters;
+
+import java.awt.Color;
+import java.awt.Composite;
+import java.awt.Graphics2D;
+import kg.apc.charting.ChartSettings;
+import org.apache.jorphan.gui.NumberRenderer;
+
+/**
+ *
+ * @author Stephane Hoblingre
+ */
+public class BarRowPlotter extends AbstractRowPlotter {
+
+   public BarRowPlotter(ChartSettings chartSettings, NumberRenderer labelRenderer) {
+      super(chartSettings, labelRenderer);
+      allowMarkers = false;
+   }
+
+   @Override
+   protected void processPoint(Graphics2D g2d, String rowLabel, Color color, int granulation) {
+      //fix bar flickering
+      if (y < chartRect.y) {
+         y = chartRect.y;
+      }
+      g2d.setColor(color);
+      if (isChartPointValid(x + 1, y)) { //as we draw bars, xMax values must be rejected
+         int x2 = chartRect.x + (int) ((calcPointX + granulation - minXVal) * dxForDVal) - x - 1;
+         Composite oldComposite = g2d.getComposite();
+         g2d.setComposite(chartSettings.getBarComposite());
+
+         int yHeight = chartRect.y + chartRect.height - y;
+
+         g2d.fillRect(x, y - 1, x2, yHeight + 1);
+         g2d.setComposite(oldComposite);
+      }
+   }
+}

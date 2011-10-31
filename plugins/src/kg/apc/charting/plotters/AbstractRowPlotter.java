10e8c4c78fed20173eca7b97ba37108e8b9032e3
Stephane.Hoblingre@gmail.com <Stephane.Hoblingre@gmail.com@173b9228-6733-11de-859f-df06e5afd5d7>
2011-10-31 08:08:19 +0000

    Preparing GraphPanelChart refactoring - it works!

--- plugins/src/kg/apc/charting/plotters/AbstractRowPlotter.java
+++ plugins/src/kg/apc/charting/plotters/AbstractRowPlotter.java
@@ -1,6 +1,7 @@
 package kg.apc.charting.plotters;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -10,6 +11,7 @@ import java.util.Map.Entry;
 import kg.apc.charting.AbstractGraphPanelChartElement;
 import kg.apc.charting.AbstractGraphRow;
 import kg.apc.charting.ChartSettings;
+import org.apache.jorphan.gui.NumberRenderer;
 
 /**
  *
@@ -17,8 +19,11 @@ import kg.apc.charting.ChartSettings;
  */
 public abstract class AbstractRowPlotter {
 
-   ChartSettings chartSettings = null;
-   Rectangle chartRect = null;
+   private final static int labelSpacing = 5;
+
+   protected ChartSettings chartSettings = null;
+   protected NumberRenderer labelRenderer = null;
+   protected Rectangle chartRect = null;
    protected long minXVal;
    protected long maxXVal;
    protected double minYVal;
@@ -26,13 +31,17 @@ public abstract class AbstractRowPlotter {
    //live values to paint rows
    protected double dxForDVal;
    protected double dyForDVal;
-
    protected int x, y;
    protected int prevX;
    protected int prevY;
+   protected int factorInUse = 1;
+   protected boolean allowMarkers = false;
 
-   public AbstractRowPlotter(ChartSettings chartSettings) {
+   protected boolean mustDrawFirstZeroingLine;
+
+   public AbstractRowPlotter(ChartSettings chartSettings, NumberRenderer labelRenderer) {
       this.chartSettings = chartSettings;
+      this.labelRenderer = labelRenderer;
    }
 
    public void setBoundsValues(Rectangle chartRect, long minXVal, long maxXVal, double minYVal, double maxYVal) {
@@ -60,37 +69,165 @@ public abstract class AbstractRowPlotter {
       return ret;
    }
 
-   public void paintRow(Graphics2D g2d, AbstractGraphRow row, String rowLabel, Color color) {
-      FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
+   //this method is responsible to maintain x, y, prevX, prevY
+   public void paintRow(Graphics2D g2d, AbstractGraphRow row, String rowLabel, Color color, double zoomFactor) {
+      
       Iterator<Entry<Long, AbstractGraphPanelChartElement>> it = row.iterator();
       Entry<Long, AbstractGraphPanelChartElement> element;
 
+      Stroke olStroke = null;
+
       prevX = -1;
       prevY = chartRect.y + chartRect.height;
+      double calcPointX = 0;
+      double calcPointY = 0;
+
+      mustDrawFirstZeroingLine = chartSettings.isDrawFinalZeroingLines();
+
+      if (row.isDrawThickLines()) {
+         olStroke = g2d.getStroke();
+         g2d.setStroke(chartSettings.getThickStroke());
+      }
 
       while (it.hasNext()) {
          if (!row.isDrawOnChart()) {
             continue;
          }
-         element = it.next();
-         processPoint(g2d, element, rowLabel, color);
+         if (factorInUse == 1) {
+
+            element = it.next();
+            AbstractGraphPanelChartElement elt = (AbstractGraphPanelChartElement) element.getValue();
+
+            //not compatible with factor != 1, ie cannot be used if limit nb of point is selected.
+            if (chartSettings.getHideNonRepValLimit() > 0) {
+               while (!elt.isPointRepresentative(chartSettings.getHideNonRepValLimit()) && it.hasNext()) {
+                  element = it.next();
+                  elt = (AbstractGraphPanelChartElement) element.getValue();
+               }
+
+               if (!elt.isPointRepresentative(chartSettings.getHideNonRepValLimit())) {
+                  break;
+               }
+            }
+
+            calcPointX = element.getKey().doubleValue();
+            calcPointY = elt.getValue();
+         } else {
+            int nbPointProcessed = 0;
+            for (int i = 0; i < factorInUse; i++) {
+               if (it.hasNext()) {
+                  element = it.next();
+                  calcPointX = calcPointX + element.getKey().doubleValue();
+                  calcPointY = calcPointY + ((AbstractGraphPanelChartElement) element.getValue()).getValue();
+                  nbPointProcessed++;
+               }
+            }
+            calcPointX = calcPointX / (double) nbPointProcessed;
+            calcPointY = calcPointY / (double) nbPointProcessed;
+         }
+
+         calcPointY = calcPointY * zoomFactor;
+
+         x = chartRect.x + (int) ((calcPointX - minXVal) * dxForDVal);
+         int yHeight = (int) ((calcPointY - minYVal) * dyForDVal);
+         y = chartRect.y + chartRect.height - yHeight;
+
+         //now x and y are set, we can call plotter
+         processPoint(g2d, rowLabel, color);
+         
+         //set prevX, prevY
+         if (isChartPointValid(x, y)) {
+            if(allowMarkers) {
+               processMarker(g2d, row, color);
+            }
+            prevX = x;
+            prevY = y;
+         }
+
+/*
+
+         //fix bar flickering
+
+         if (y < chartRect.y && row.isDrawBar()) {
+            y = chartRect.y;
+         }
+
+         
+         boolean valid = isChartPointValid(x, y);
+
+         // draw lines
+         if (row.isDrawLine() && chartSettings.getChartType() == ChartSettings.CHART_TYPE_DEFAULT || chartSettings.getChartType() == ChartSettings.CHART_TYPE_LINE) {
+            if (mustDrawFirstZeroingLine && valid) {
+               mustDrawFirstZeroingLine = false;
+               prevX = x;
+            }
+
+            isPointBellowForcedMaxY = y >= chartRect.y;
+
+            if (prevX >= 0) {
+               g.setColor(color);
+               if (valid) {
+                  if (prevY >= chartRect.y && y >= chartRect.y) {
+                     g.drawLine(prevX, prevY, x, y);
+                  } else if (prevY >= chartRect.y && y < chartRect.y) {
+                     int x1 = (x - prevX) * (chartRect.y - prevY) / (y - prevY) + prevX;
+                     g.drawLine(prevX, prevY, x1, chartRect.y);
+                  } else if (prevY < chartRect.y && y >= chartRect.y) {
+                     int x1 = (x - prevX) * (chartRect.y - prevY) / (y - prevY) + prevX;
+                     g.drawLine(x1, chartRect.y, x, y);
+                  }
+               }
+            }
+            if (valid) {
+               prevX = x;
+               prevY = y;
+            }
+         }
+
+         // draw bars
+         if (row.isDrawBar() && chartSettings.getChartType() == ChartSettings.CHART_TYPE_DEFAULT || chartSettings.getChartType() == ChartSettings.CHART_TYPE_BAR) {
+            g.setColor(color);
+            if (isChartPointValid(x + 1, y)) //as we draw bars, xMax values must be rejected
+            {
+               int x2 = chartRect.x + (int) ((calcPointX + row.getGranulationValue() - minXVal) * dxForDVal) - x - 1;
+               Composite oldComposite = ((Graphics2D) g).getComposite();
+               ((Graphics2D) g).setComposite(barComposite);
+
+               g.fillRect(x, y - 1, x2, yHeight + 1);
+               ((Graphics2D) g).setComposite(oldComposite);
+            }
+         }
+
+*/
+
+      if (row.isDrawValueLabel() && isChartPointValid(x, y) && y >= chartRect.y) {
+            drawLabels(g2d, row, calcPointY);
+         }
       }
 
-      postPaintRow();
+      processFinalLines(row, g2d, color);
+      postPaintRow(row, g2d, color);
+
+      if(olStroke != null) {
+         g2d.setStroke(olStroke);
+      }
    }
 
-   protected abstract void processPoint(Graphics2D g2d, Entry<Long, AbstractGraphPanelChartElement> element, String rowLabel, Color color);
+   protected abstract void processPoint(Graphics2D g2d, String rowLabel, Color color);
 
-   protected void postPaintRow() {
-
+   protected void postPaintRow(AbstractGraphRow row, Graphics2D g2d, Color color) {
    }
 
-   protected void drawMarker(Graphics2D g2d, AbstractGraphRow row, Color color, int x, int y) {
+   private void processMarker(Graphics2D g2d, AbstractGraphRow row, Color color) {
       int radius = row.getMarkerSize();
 
       //check if forced via settings
-      if(chartSettings.getChartMarkers() == ChartSettings.CHART_MARKERS_YES) radius = Math.max(AbstractGraphRow.MARKER_SIZE_SMALL, row.getMarkerSize());
-      if(chartSettings.getChartMarkers() == ChartSettings.CHART_MARKERS_NO) radius = AbstractGraphRow.MARKER_SIZE_NONE;
+      if (chartSettings.getChartMarkers() == ChartSettings.CHART_MARKERS_YES) {
+         radius = Math.max(AbstractGraphRow.MARKER_SIZE_SMALL, row.getMarkerSize());
+      }
+      if (chartSettings.getChartMarkers() == ChartSettings.CHART_MARKERS_NO) {
+         radius = AbstractGraphRow.MARKER_SIZE_NONE;
+      }
 
       // draw markers
       if (radius != AbstractGraphRow.MARKER_SIZE_NONE && (y >= chartRect.y)) {
@@ -101,7 +238,29 @@ public abstract class AbstractRowPlotter {
       }
    }
 
-   protected void drawFinalLines(AbstractGraphRow row, Graphics2D g2d, int prevX, int prevY, Color color) {
+   private void drawLabels(Graphics2D g2d, AbstractGraphRow row, double yValue) {
+      g2d.setColor(Color.DARK_GRAY);
+      Font oldFont = g2d.getFont();
+      g2d.setFont(g2d.getFont().deriveFont(Font.BOLD));
+
+      FontMetrics fm = g2d.getFontMetrics(g2d.getFont());
+
+      labelRenderer.setValue(yValue);
+      int labelSize = g2d.getFontMetrics(g2d.getFont()).stringWidth(labelRenderer.getText());
+      //if close to end
+      if (x + row.getMarkerSize() + labelSpacing + labelSize > chartRect.x + chartRect.width) {
+         g2d.drawString(labelRenderer.getText(),
+                 x - row.getMarkerSize() - labelSpacing - labelSize,
+                 y + fm.getAscent() / 2);
+      } else {
+         g2d.drawString(labelRenderer.getText(),
+                 x + row.getMarkerSize() + labelSpacing,
+                 y + fm.getAscent() / 2);
+      }
+      g2d.setFont(oldFont);
+   }
+
+   private void processFinalLines(AbstractGraphRow row, Graphics2D g2d, Color color) {
       Stroke oldStroke = null;
 
       if (row.isDrawLine() && chartSettings.isDrawFinalZeroingLines()) {
--- plugins/src/kg/apc/charting/plotters/LineRowPlotter.java
+++ plugins/src/kg/apc/charting/plotters/LineRowPlotter.java
@@ -2,9 +2,8 @@ package kg.apc.charting.plotters;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.util.Map.Entry;
-import kg.apc.charting.AbstractGraphPanelChartElement;
 import kg.apc.charting.ChartSettings;
+import org.apache.jorphan.gui.NumberRenderer;
 
 /**
  *
@@ -12,13 +11,31 @@ import kg.apc.charting.ChartSettings;
  */
 public class LineRowPlotter extends AbstractRowPlotter {
 
-   public LineRowPlotter(ChartSettings chartSettings) {
-      super(chartSettings);
+   public LineRowPlotter(ChartSettings chartSettings, NumberRenderer labelRenderer) {
+      super(chartSettings, labelRenderer);
+      allowMarkers = true;
    }
 
    @Override
-   protected void processPoint(Graphics2D g2d, Entry<Long, AbstractGraphPanelChartElement> element, String rowLabel, Color color) {
-      throw new UnsupportedOperationException("Not supported yet.");
+   protected void processPoint(Graphics2D g2d, String rowLabel, Color color) {
+      boolean valid = isChartPointValid(x, y);
+      if (mustDrawFirstZeroingLine && valid) {
+         mustDrawFirstZeroingLine = false;
+         prevX = x;
+      }
+      if (prevX >= 0) {
+         g2d.setColor(color);
+         if (valid) {
+            if (prevY >= chartRect.y && y >= chartRect.y) {
+               g2d.drawLine(prevX, prevY, x, y);
+            } else if (prevY >= chartRect.y && y < chartRect.y) {
+               int x1 = (x - prevX) * (chartRect.y - prevY) / (y - prevY) + prevX;
+               g2d.drawLine(prevX, prevY, x1, chartRect.y);
+            } else if (prevY < chartRect.y && y >= chartRect.y) {
+               int x1 = (x - prevX) * (chartRect.y - prevY) / (y - prevY) + prevX;
+               g2d.drawLine(x1, chartRect.y, x, y);
+            }
+         }
+      }
    }
-
 }

dbbc02bc240570d18a7cc85485e6eabb7b3ea44a
Andrey Pohilko <undera@yandex-team.ru>
2013-03-30 23:51:20 +0400

    Update unit tests

--- test/RootSuite.java
+++ test/RootSuite.java
@@ -1,8 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
+import com.ComSuite;
+import kg.KgSuite;
+import org.OrgSuite;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -15,27 +14,22 @@ import org.junit.runners.Suite;
  * @author APC
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({kg.KgSuite.class})
+@Suite.SuiteClasses({ComSuite.class, OrgSuite.class, KgSuite.class})
 public class RootSuite {
 
-   @BeforeClass
-   public static void setUpClass() throws Exception
-   {
-   }
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
 
-   @AfterClass
-   public static void tearDownClass() throws Exception
-   {
-   }
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
 
-   @Before
-   public void setUp() throws Exception
-   {
-   }
+    @Before
+    public void setUp() throws Exception {
+    }
 
-   @After
-   public void tearDown() throws Exception
-   {
-   }
-
+    @After
+    public void tearDown() throws Exception {
+    }
 }
---
+++ test/com/atlantbh/jmeter/plugins/hbasecomponents/HbasecomponentsSuite.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 undera.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlantbh.jmeter.plugins.hbasecomponents;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author undera
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({com.atlantbh.jmeter.plugins.hbasecomponents.samplers.SamplersSuite.class, com.atlantbh.jmeter.plugins.hbasecomponents.config.ConfigSuite.class, com.atlantbh.jmeter.plugins.hbasecomponents.utils.UtilsSuite.class})
+public class HbasecomponentsSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}

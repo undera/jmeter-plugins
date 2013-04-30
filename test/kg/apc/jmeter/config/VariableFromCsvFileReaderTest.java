8d00a2674f8f94ce033c941d970eb4a42dac495f
Matthew Fremont <matthew.fremont@server.fake>
2013-04-30 11:40:27 -0400

    whitespace cleanup

--- test/kg/apc/jmeter/config/VariableFromCsvFileReaderTest.java
+++ test/kg/apc/jmeter/config/VariableFromCsvFileReaderTest.java
@@ -3,15 +3,16 @@ package kg.apc.jmeter.config;
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.util.Map;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- *
  * @author Stephane Hoblingre
  */
 public class VariableFromCsvFileReaderTest {
@@ -19,16 +20,16 @@ public class VariableFromCsvFileReaderTest {
     private final String fileName;
 
     public VariableFromCsvFileReaderTest() {
-       fileName = TestCsvFileActionTest.class.getResource("csvFileTest.csv").getPath();
+        fileName = TestCsvFileActionTest.class.getResource("csvFileTest.csv").getPath();
     }
 
-   @BeforeClass
-   public static void setUpClass() throws Exception {
-   }
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
 
-   @AfterClass
-   public static void tearDownClass() throws Exception {
-   }
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
 
     @Before
     public void setUp() {
@@ -38,18 +39,18 @@ public class VariableFromCsvFileReaderTest {
     public void tearDown() {
     }
 
-   /**
-    * Test of getDataAsMap method, of class VariableFromCsvFileReader.
-    */
-   @Test
-   public void testGetDataAsMap() {
-      System.out.println("getDataAsMap");
-      String prefix = "";
-      String separator = ",";
-      VariableFromCsvFileReader instance = new VariableFromCsvFileReader(fileName);
-      Map result = instance.getDataAsMap(prefix, separator);
-      assertTrue(result.size() == 2);
-   }
+    /**
+     * Test of getDataAsMap method, of class VariableFromCsvFileReader.
+     */
+    @Test
+    public void testGetDataAsMap() {
+        System.out.println("getDataAsMap");
+        String prefix = "";
+        String separator = ",";
+        VariableFromCsvFileReader instance = new VariableFromCsvFileReader(fileName);
+        Map result = instance.getDataAsMap(prefix, separator);
+        assertTrue(result.size() == 2);
+    }
 
     /**
      * Test of getDataAsMap using a BufferedReader as input instead of a named file.

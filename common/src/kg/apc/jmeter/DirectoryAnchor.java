34d899c4337773e051b81c8d99df10eb58e91647
Andrey Pohilko <undera@yandex-team.ru>
2013-07-04 21:33:13 +0400

    Fix unit tests

--- common/src/kg/apc/jmeter/DirectoryAnchor.java
+++ common/src/kg/apc/jmeter/DirectoryAnchor.java
@@ -8,7 +8,7 @@ public class DirectoryAnchor {
 
     @Override
     public String toString() {
-        String file = this.getClass().getResource("short.jtl").getPath();
+        String file = this.getClass().getResource("version.properties").getPath();
         return file.substring(0, file.lastIndexOf("/"));
     }
 }

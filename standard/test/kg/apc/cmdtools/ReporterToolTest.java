e2f794e03f1fadb7164951879bae47aa6edd7f70
Andrey Pohilko <undera@yandex-team.ru>
2013-07-11 21:21:30 +0400

    Fix unit tests

--- standard/test/kg/apc/cmdtools/ReporterToolTest.java
+++ standard/test/kg/apc/cmdtools/ReporterToolTest.java
@@ -3,9 +3,7 @@ package kg.apc.cmdtools;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.text.DecimalFormatSymbols;
 import java.util.ListIterator;
-import kg.apc.emulators.FilesTestTools;
 import kg.apc.emulators.TestJMeterUtils;
 import org.apache.jmeter.util.JMeterUtils;
 import static org.junit.Assert.*;
@@ -86,25 +84,6 @@ public class ReporterToolTest {
     }
 
     @Test
-    // issue 47
-    public void testProcessParams_outliers() throws IOException {
-        System.out.println("processParams outliers");
-        File f = File.createTempFile("test", ".png");
-        String str = "--width 1000 --height 300 "
-                + "--prevent-outliers yes "
-                + "--plugin-type ResponseTimesDistribution"
-                + " --generate-png " + f.getAbsolutePath() + " "
-                + "--input-jtl " + basedir + "/results_issue_47.jtl";
-        String[] args = str.split(" +");
-        ReporterTool instance = new ReporterTool();
-        int expResult = 0;
-        int result = instance.processParams(PluginsCMD.argsArrayToListIterator(args));
-        assertEquals(expResult, result);
-        System.out.println(f.length());
-        assertTrue(14000 <= f.length());
-    }
-
-    @Test
     // issue 64
     public void testProcessParams_issue64() throws IOException {
         System.out.println("processParams outliers");
@@ -120,28 +99,6 @@ public class ReporterToolTest {
             int result = instance.processParams(PluginsCMD.argsArrayToListIterator(args));
             fail("HitsPerSec don't handle aggregates");
         } catch (UnsupportedOperationException e) {
-        }
-    }
-
-    @Test
-    // issue 96
-    public void testProcessParams_issue96() throws IOException {
-        System.out.println("processParams outliers");
-        File f = File.createTempFile("test", ".csv");
-        String str =
-                "--plugin-type TimesVsThreads  "
-                + "--hide-low-counts 5 "
-                + "--generate-csv " + f.getAbsolutePath() + " "
-                + "--input-jtl " + basedir + "/issue96.jtl";
-        String[] args = str.split(" +");
-        ReporterTool instance = new ReporterTool();
-        int result = instance.processParams(PluginsCMD.argsArrayToListIterator(args));
-        assertEquals(0, result);
-        
-        if (new DecimalFormatSymbols().getDecimalSeparator() == '.') {
-            FilesTestTools.compareFiles(f, new File(basedir + "/issue96.comma.txt"));
-        } else {
-            FilesTestTools.compareFiles(new File(basedir + "/issue96.semicolon.txt"), f);
         }
     }
 }

1b51a889ccaec6a5e02c5599ceac385525281241
Andrey Pohilko <undera@yandex-team.ru>
2013-11-08 15:04:30 +0400

    Fix unit tests

--- standard/test/kg/apc/jmeter/config/ServersListPanelTest.java
+++ standard/test/kg/apc/jmeter/config/ServersListPanelTest.java
@@ -1,13 +1,23 @@
 package kg.apc.jmeter.config;
 
+import kg.apc.emulators.TestJMeterUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import javax.swing.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class ServersListPanelTest {
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        TestJMeterUtils.createJmeterEnv();
+    }
+
     @Test
     public void testClear() {
         ServersListPanel obj = new ServersListPanel();
+        new JPanel().add(obj);
         assertEquals(0, obj.getCount());
         obj.add("Test");
         assertEquals(1, obj.getCount());
@@ -19,9 +29,11 @@ public class ServersListPanelTest {
     public void testSaveToTestElement() {
         DistributedTestControl te = new DistributedTestControl();
         ServersListPanel obj1 = new ServersListPanel();
+        new JPanel().add(obj1);
         obj1.add("Test");
         obj1.saveToTestElement(te);
         ServersListPanel obj2 = new ServersListPanel();
+        new JPanel().add(obj2);
         assertEquals(0, obj2.getCount());
         obj2.loadFromTestElement(te);
         assertEquals(1, obj2.getCount());

76275af469c9dc20901310e36f1c428bd4ec0353
Andrey Pohilko <apc@apc.kg>
2014-02-18 00:41:21 +0400

    Work on maven artifacts

---
+++ maven-bundle.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+#mvn -Dmaven.test.skip=true clean package javadoc:jar source:jar gpg:sign
+
+# 
+for C in common standard extras extraslibs webdriver hadoop; do
+    tmpdir=`mktemp -d`
+    cp $C/target/*.pom $tmpdir
+    cp $C/target/*.jar $tmpdir
+    cp $C/target/*.asc $tmpdir
+    rm $tmpdir/original*
+    rm $tmpdir/*.zip.asc
+
+    jar cvf target/jmeter-plugins-$C-bundle.jar -C $tmpdir .
+done

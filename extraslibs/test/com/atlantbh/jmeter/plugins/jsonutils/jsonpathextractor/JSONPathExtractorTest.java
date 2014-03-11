d2dae1c8a9fd6bc784745c0ddef7eed54ad59b49
Patrick Haas <patrick@jphaas.net>
2014-03-11 13:44:54 -0700

    reverted auto-formatter havoc

--- extraslibs/test/com/atlantbh/jmeter/plugins/jsonutils/jsonpathextractor/JSONPathExtractorTest.java
+++ extraslibs/test/com/atlantbh/jmeter/plugins/jsonutils/jsonpathextractor/JSONPathExtractorTest.java
@@ -25,39 +25,41 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class JSONPathExtractorTest {
-
     private static final String json = "{ \"store\": {\n" +
-             "    \"book\": [ \n" +
-             "      { \"category\": \"reference\",\n" +
-             "        \"author\": \"Nigel Rees\",\n" +
-             "        \"title\": \"Sayings of the Century\",\n" +
-             "        \"price\": 8.95\n" +
-             "      },\n" +
-             "      { \"category\": \"fiction\",\n" +
-             "        \"author\": \"Evelyn Waugh\",\n" +
-             "        \"title\": \"Sword of Honour\",\n" +
-             "        \"price\": 12.99\n" +
-             "      },\n" +
-             "      { \"category\": \"fiction\",\n" +
-             "        \"author\": \"Herman Melville\",\n" +
-             "        \"title\": \"Moby Dick\",\n" +
-             "        \"isbn\": \"0553213113\",\n" +
-             "        \"price\": 8.99\n" +
-             "      },\n" +
-             "      { \"category\": \"fiction\",\n" +
-             "        \"author\": \"J. R. R. Tolkien\",\n" +
-             "        \"title\": \"The Lord of the Rings\",\n" +
-             "        \"isbn\": \"0395193958\",\n" +
-             "        \"price\": 22.99\n" +
-             "      }\n" +
-             "    ],\n" +
-             "    \"bicycle\": {\n" +
-             "      \"color\": \"red\",\n" +
-             "      \"price\": 19.95\n" +
-             "    }\n" +
-             "  }\n" +
-             "}";
-	private static final String jsonArray = "[" + "{\"id\": \"123\", \"value\": 123}," + "{\"id\": \"456\", \"value\": \"String\"}," + "]";
+            "    \"book\": [ \n" +
+            "      { \"category\": \"reference\",\n" +
+            "        \"author\": \"Nigel Rees\",\n" +
+            "        \"title\": \"Sayings of the Century\",\n" +
+            "        \"price\": 8.95\n" +
+            "      },\n" +
+            "      { \"category\": \"fiction\",\n" +
+            "        \"author\": \"Evelyn Waugh\",\n" +
+            "        \"title\": \"Sword of Honour\",\n" +
+            "        \"price\": 12.99\n" +
+            "      },\n" +
+            "      { \"category\": \"fiction\",\n" +
+            "        \"author\": \"Herman Melville\",\n" +
+            "        \"title\": \"Moby Dick\",\n" +
+            "        \"isbn\": \"0-553-21311-3\",\n" +
+            "        \"price\": 8.99\n" +
+            "      },\n" +
+            "      { \"category\": \"fiction\",\n" +
+            "        \"author\": \"J. R. R. Tolkien\",\n" +
+            "        \"title\": \"The Lord of the Rings\",\n" +
+            "        \"isbn\": \"0-395-19395-8\",\n" +
+            "        \"price\": 22.99\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"bicycle\": {\n" +
+            "      \"color\": \"red\",\n" +
+            "      \"price\": 19.95\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+    private static final String jsonArray = "[" +
+        	"	{\"id\": \"123\", \"value\": 123}," +
+    		"	{\"id\": \"456\", \"value\": \"String\"}," +
+    		"]";
 
 	@Before
 	public void setUpClass() throws Exception {

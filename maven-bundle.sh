639657f8de3a9d7b2bd9abe4216dcfe015492085
Andrey Pohilko <apc4@ya.ru>
2015-03-13 20:27:50 +0300

    Fix bundle script

--- maven-bundle.sh
+++ maven-bundle.sh
@@ -1,15 +1,18 @@
-#! /bin/sh
+#! /bin/sh -xe
 
-mvn -Dmaven.test.skip=true clean package javadoc:jar source:jar gpg:sign
+mvn -Dmaven.test.skip=false clean package javadoc:jar source:jar gpg:sign
 
+mkdir -p bundle
 # 
 for C in . common standard extras extraslibs webdriver hadoop; do
     tmpdir=`mktemp -d`
+    if ls $C/target/*.jar 1> /dev/null 2>&1; then
+        cp $C/target/*.jar $tmpdir
+    fi
     cp $C/target/*.pom $tmpdir
-    cp $C/target/*.jar $tmpdir
     cp $C/target/*.asc $tmpdir
-    rm $tmpdir/original*
-    rm $tmpdir/*.zip.asc
+    rm -f $tmpdir/original*
+    rm -f $tmpdir/*.zip.asc
 
-    jar cvf target/jmeter-plugins-$C-bundle.jar -C $tmpdir .
+    jar cvf bundle/jmeter-plugins-$C-bundle.jar -C $tmpdir .
 done

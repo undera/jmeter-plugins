689dfbf409c7f76d564c5d5b4cdf3aef24d1a619
Andrey Pohilko <apc4@ya.ru>
2015-06-17 14:07:00 +0300

    Reformat the code

--- webdriver/src/com/googlecode/jmeter/plugins/webdriver/sampler/gui/WebDriverSamplerGui.java
+++ webdriver/src/com/googlecode/jmeter/plugins/webdriver/sampler/gui/WebDriverSamplerGui.java
@@ -17,20 +17,20 @@ public class WebDriverSamplerGui extends AbstractSamplerGui {
     private static final Logger LOGGER = LoggingManager.getLoggerForClass();
 
     static {
-        if(!GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance()) {
+        if (!GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance()) {
             DefaultSyntaxKit.initKit();
         } else {
             LOGGER.info("Headless environment detected. Disabling JSyntaxPane highlighting.");
         }
     }
 
-	JTextField parameters;
+    JTextField parameters;
 
-	JEditorPane script;
+    JEditorPane script;
 
-	public WebDriverSamplerGui() {
-		createGui();
-	}
+    public WebDriverSamplerGui() {
+        createGui();
+    }
 
     @Override
     public String getStaticLabel() {
@@ -49,20 +49,20 @@ public class WebDriverSamplerGui extends AbstractSamplerGui {
         super.configure(element);
     }
 
-	@Override
-	public TestElement createTestElement() {
+    @Override
+    public TestElement createTestElement() {
         WebDriverSampler sampler = new WebDriverSampler();
-		modifyTestElement(sampler);
-		return sampler;
-	}
+        modifyTestElement(sampler);
+        return sampler;
+    }
 
-	@Override
-	public void modifyTestElement(TestElement element) {
-		element.clear();
-		this.configureTestElement(element);
-		element.setProperty(WebDriverSampler.SCRIPT, script.getText());
-		element.setProperty(WebDriverSampler.PARAMETERS, parameters.getText());
-	}
+    @Override
+    public void modifyTestElement(TestElement element) {
+        element.clear();
+        this.configureTestElement(element);
+        element.setProperty(WebDriverSampler.SCRIPT, script.getText());
+        element.setProperty(WebDriverSampler.PARAMETERS, parameters.getText());
+    }
 
     @Override
     public void clearGui() {
@@ -72,54 +72,56 @@ public class WebDriverSamplerGui extends AbstractSamplerGui {
         script.setText(""); //$NON-NLS-1$
     }
 
-	private void createGui() {
-		setLayout(new BorderLayout(0, 5));
-		setBorder(makeBorder());
+    private void createGui() {
+        setLayout(new BorderLayout(0, 5));
+        setBorder(makeBorder());
 
-		Box box = Box.createVerticalBox();
-		box.add(JMeterPluginsUtils.addHelpLinkToPanel(makeTitlePanel(), "WebDriverSampler"));
-		box.add(createParameterPanel());
-		add(box, BorderLayout.NORTH);
+        Box box = Box.createVerticalBox();
+        box.add(JMeterPluginsUtils.addHelpLinkToPanel(makeTitlePanel(), "WebDriverSampler"));
+        box.add(createParameterPanel());
+        add(box, BorderLayout.NORTH);
 
-		JPanel panel = createScriptPanel();
-		add(panel, BorderLayout.CENTER);
-		// Don't let the input field shrink too much
-		add(Box.createVerticalStrut(panel.getPreferredSize().height),
-				BorderLayout.WEST);
-	}
+        JPanel panel = createScriptPanel();
+        add(panel, BorderLayout.CENTER);
+        // Don't let the input field shrink too much
+        add(Box.createVerticalStrut(panel.getPreferredSize().height),
+                BorderLayout.WEST);
+    }
 
-	private JPanel createParameterPanel() {
-		final JLabel label = new JLabel("Parameters");
+    private JPanel createParameterPanel() {
+        final JLabel label = new JLabel("Parameters");
 
-		parameters = new JTextField(10);
-		parameters.setName(WebDriverSampler.PARAMETERS);
-		label.setLabelFor(parameters);
+        parameters = new JTextField(10);
+        parameters.setName(WebDriverSampler.PARAMETERS);
+        label.setLabelFor(parameters);
 
-		final JPanel parameterPanel = new JPanel(new BorderLayout(5, 0));
-		parameterPanel.add(label, BorderLayout.WEST);
-		parameterPanel.add(parameters, BorderLayout.CENTER);
-		return parameterPanel;
-	}
+        final JPanel parameterPanel = new JPanel(new BorderLayout(5, 0));
+        parameterPanel.add(label, BorderLayout.WEST);
+        parameterPanel.add(parameters, BorderLayout.CENTER);
 
-	private JPanel createScriptPanel() {
-		script = new JEditorPane();
+
+        return parameterPanel;
+    }
+
+    private JPanel createScriptPanel() {
+        script = new JEditorPane();
         final JScrollPane scrollPane = new JScrollPane(script);
         script.setContentType("text/javascript");
         script.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
 
         final JLabel label = new JLabel("Script (see below for variables that are defined)");
-		label.setLabelFor(script);
+        label.setLabelFor(script);
 
-		final JPanel panel = new JPanel(new BorderLayout());
-		panel.add(label, BorderLayout.NORTH);
+        final JPanel panel = new JPanel(new BorderLayout());
+        panel.add(label, BorderLayout.NORTH);
         panel.add(scrollPane, BorderLayout.CENTER);
 
-		final JTextArea explain = new JTextArea("The following variables are defined for the script: WDS.name, WDS.parameters, WDS.args, WDS.log, WDS.browser, WDS.sampleResult");
-		explain.setLineWrap(true);
-		explain.setEditable(false);
-		explain.setBackground(this.getBackground());
-		panel.add(explain, BorderLayout.SOUTH);
+        final JTextArea explain = new JTextArea("The following variables are defined for the script: WDS.name, WDS.parameters, WDS.args, WDS.log, WDS.browser, WDS.sampleResult");
+        explain.setLineWrap(true);
+        explain.setEditable(false);
+        explain.setBackground(this.getBackground());
+        panel.add(explain, BorderLayout.SOUTH);
 
-		return panel;
-	}
+        return panel;
+    }
 }

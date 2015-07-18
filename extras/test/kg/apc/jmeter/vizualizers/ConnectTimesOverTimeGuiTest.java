7d5932a2d5f06ca226d97b93e04f341c24cb772b
Andrey Pohilko <apc4@ya.ru>
2015-07-18 22:00:27 +0300

    Add unit test

---
+++ extras/test/kg/apc/jmeter/vizualizers/ConnectTimesOverTimeGuiTest.java
@@ -0,0 +1,28 @@
+package kg.apc.jmeter.vizualizers;
+
+import kg.apc.emulators.TestJMeterUtils;
+import org.apache.jmeter.samplers.SampleResult;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConnectTimesOverTimeGuiTest {
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        TestJMeterUtils.createJmeterEnv();
+    }
+
+    @Test
+    public void testDummies() {
+        ConnectTimesOverTimeGui obj = new ConnectTimesOverTimeGui();
+        obj.getLabelResource();
+    }
+
+    @Test
+    public void testAdd() {
+        ConnectTimesOverTimeGui obj = new ConnectTimesOverTimeGui();
+        SampleResult res=new SampleResult();
+        obj.add(res);
+    }
+}

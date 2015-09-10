d8fc127b847cd2c5c1aced6754d15cacdb9912f8
Flavio Cysne <flaviocysne@gmail.com>
2015-09-10 09:48:19 -0300

    Add tests to PhantomJS (GhostDriver) capability
    Test PhantomJS (GhostDriver) capability

--- webdriver/test/com/googlecode/jmeter/plugins/webdriver/config/RemoteDesiredCapabilitiesFactoryTest.java
+++ webdriver/test/com/googlecode/jmeter/plugins/webdriver/config/RemoteDesiredCapabilitiesFactoryTest.java
@@ -10,26 +10,34 @@ import static org.hamcrest.core.Is.*;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class RemoteDesiredCapabilitiesFactoryTest {
-	
+
 	@Test
 	public void shouldReturnFirefoxDriverWhenFirefoxCapabilityIsPassed() throws Exception {
 		DesiredCapabilities capabilities = RemoteDesiredCapabilitiesFactory.build(RemoteCapability.FIREFOX);
 		assertThat(capabilities.getCapability(FirefoxDriver.PROFILE), is(notNullValue()));
 		assertThat(capabilities.getBrowserName(), is("firefox"));
 	}
-	
+
 	@Test
 	public void shouldReturnChromeDriverWhenChromeCapabilityIsPassed() throws Exception {
 		DesiredCapabilities capabilities = RemoteDesiredCapabilitiesFactory.build(RemoteCapability.CHROME);
 		assertThat(capabilities.getCapability(ChromeOptions.CAPABILITY), is(notNullValue()));
 		assertThat(capabilities.getBrowserName(), is("chrome"));
 	}
-	
+
 	@Test
 	public void shouldReturnInternetExplorerDriverWhenChromeCapabilityIsPassed() throws Exception {
 		DesiredCapabilities capabilities = RemoteDesiredCapabilitiesFactory.build(RemoteCapability.INTERNET_EXPLORER);
 		assertThat(capabilities.getBrowserName(), is("internet explorer"));
 	}
+
+	@Test
+	public void shouldReturnPhantomJSDriverWhenChromeCapabilityIsPassed() throws Exception {
+		DesiredCapabilities capabilities = RemoteDesiredCapabilitiesFactory.build(RemoteCapability.PHANTOMJS);
+		assertThat(capabilities.getCapability(ChromeOptions.CAPABILITY), is(notNullValue()));
+		assertThat(capabilities.getBrowserName(), is("phantomjs"));
+	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldThrowAnExceptionWhenAnInvalidCapabilityIsPassed() throws Exception {
 		RemoteDesiredCapabilitiesFactory.build(null);

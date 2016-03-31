abd848ccab1542b436b813f91f00d81c6e5b54d5
Andrey Pohilko <apc4@ya.ru>
2016-03-31 10:15:53 +0300

    Just fix files

---
+++ common/src/org/jmeterplugins/repository/PluginManagerDialog.java
@@ -0,0 +1,26 @@
+package org.jmeterplugins.repository;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PluginManagerDialog extends JDialog implements ChangeListener, ActionListener, HyperlinkListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
+
+    @Override
+    public void hyperlinkUpdate(HyperlinkEvent e) {
+
+    }
+}

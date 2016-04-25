51b6dd9b2ff1ca2bd9bdaecabb221155d60e5a27
Andrey Pohilko <apc4@ya.ru>
2016-04-25 14:51:58 +0300

    cosmetics

--- site/JPGC/PluginsRepository.php
+++ site/JPGC/PluginsRepository.php
@@ -7,25 +7,25 @@ class PluginsRepository extends \PWE\Modules\PWEModule implements \PWE\Modules\O
 {
     public function process()
     {
-            if ($_POST['stats']!='null') {
-                \PWE\Core\PWELogger::warn("Status: %s", $_POST['stats']);
-            }
-            $node = $this->PWE->getNode();
-            $configsDir = $node['!a']['configs'];
-            if (!$configsDir || !is_dir($configsDir)) {
-                throw new \PWE\Exceptions\HTTP5xxException("Configs dir don't exist: " . realpath($configsDir));
-            }
+        if ($_POST['stats'] && $_POST['stats'] != 'null') {
+            \PWE\Core\PWELogger::warn("Status: %s", $_POST['stats']);
+        }
+        $node = $this->PWE->getNode();
+        $configsDir = $node['!a']['configs'];
+        if (!$configsDir || !is_dir($configsDir)) {
+            throw new \PWE\Exceptions\HTTP5xxException("Configs dir don't exist: " . realpath($configsDir));
+        }
 
-            $plugins = [];
-            foreach (scandir($configsDir) as $fname) {
-                if ($fname[0] == '.') {
-                    continue;
-                }
-                $set = json_decode(file_get_contents($configsDir . '/' . $fname), true);
-                $plugins = array_merge($plugins, $set);
+        $plugins = [];
+        foreach (scandir($configsDir) as $fname) {
+            if ($fname[0] == '.') {
+                continue;
             }
+            $set = json_decode(file_get_contents($configsDir . '/' . $fname), true);
+            $plugins = array_merge($plugins, $set);
+        }
 
-            $this->PWE->sendHTTPHeader("Content-Type: application/json");
-            throw new \PWE\Exceptions\HTTP2xxException(json_encode($plugins));
+        $this->PWE->sendHTTPHeader("Content-Type: application/json");
+        throw new \PWE\Exceptions\HTTP2xxException(json_encode($plugins));
     }
 }

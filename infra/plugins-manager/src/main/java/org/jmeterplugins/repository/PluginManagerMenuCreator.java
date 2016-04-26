a28fcf76d31feb523622445dd2990d6815fc79d5
Andrey Pohilko <apc4@ya.ru>
2016-04-26 10:37:30 +0300

    Don't fail jmeter launch on pmgr failure

--- infra/plugins-manager/src/main/java/org/jmeterplugins/repository/PluginManagerMenuCreator.java
+++ infra/plugins-manager/src/main/java/org/jmeterplugins/repository/PluginManagerMenuCreator.java
@@ -1,17 +1,24 @@
 package org.jmeterplugins.repository;
 
 import org.apache.jmeter.gui.plugin.MenuCreator;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 
 import javax.swing.*;
 
 public class PluginManagerMenuCreator implements MenuCreator {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
     @Override
     public JMenuItem[] getMenuItemsAtLocation(MENU_LOCATION location) {
         if (location == MENU_LOCATION.OPTIONS) {
-            JMenuItem item = new PluginManagerMenuItem();
-            JMenuItem[] arr = new JMenuItem[1];
-            arr[0] = item;
-            return arr;
+            try {
+                return new JMenuItem[]{new PluginManagerMenuItem()};
+            } catch (Throwable e) {
+                log.error("Failed to load Plugins Manager", e);
+                return new JMenuItem[0];
+            }
+
         } else {
             return new JMenuItem[0];
         }

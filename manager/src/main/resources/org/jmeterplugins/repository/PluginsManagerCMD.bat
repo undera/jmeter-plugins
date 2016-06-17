7c165282e92caa818bb3e45bc5816005c80079ca
Andrey Pokhilko <apc4@ya.ru>
2016-06-17 16:16:22 +0300

    Pmgr proxy (#110)
    * Rework to use HTTPClient and take proxy settings
    
    * make JVM_ARGS work with pmgr cmd
    
    * Document proxy usage

--- manager/src/main/resources/org/jmeterplugins/repository/PluginsManagerCMD.bat
+++ manager/src/main/resources/org/jmeterplugins/repository/PluginsManagerCMD.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -jar %~dp0\..\lib\cmdrunner-2.0.jar --tool org.jmeterplugins.repository.PluginManagerCMD %*
+java %JVM_ARGS% -jar %~dp0\..\lib\cmdrunner-2.0.jar --tool org.jmeterplugins.repository.PluginManagerCMD %*

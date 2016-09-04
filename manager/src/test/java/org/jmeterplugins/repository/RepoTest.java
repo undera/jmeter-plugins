fbb2efe315635e41f444975b7600d4f9c4079c9d
Andrey Pohilko <apc4@ya.ru>
2016-09-04 17:37:06 +0300

    preparing local repo struct

--- manager/src/test/java/org/jmeterplugins/repository/RepoTest.java
+++ manager/src/test/java/org/jmeterplugins/repository/RepoTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,7 +16,8 @@ import java.util.*;
 public class RepoTest {
     private final Set<String> cache = new HashSet<>();
     private String s = File.separator;
-    private File lib = new File(System.getProperty("project.build.directory", "target") + s + "jars" + s + "lib");
+    private File repo = new File(System.getProperty("project.build.directory", "target") + s + "jpgc-repo");
+    private File lib = new File(repo.getAbsolutePath() + s + "lib");
     private File libExt = new File(lib.getAbsolutePath() + s + "ext");
 
     public RepoTest() {
@@ -36,6 +38,37 @@ public class RepoTest {
         }
 
         List<String> problems = new ArrayList<>();
+        File[] files = getRepoFiles();
+
+        JSONArray merged = new JSONArray();
+        for (File repoFile : files) {
+            System.out.println("Checking repo: " + repoFile.getCanonicalPath());
+            String content = new String(Files.readAllBytes(Paths.get(repoFile.getAbsolutePath())), "UTF-8");
+            JSON json = JSONSerializer.toJSON(content, new JsonConfig());
+            JSONArray list = (JSONArray) json;
+            for (Object item : list) {
+                JSONObject spec = (JSONObject) item;
+                checkPlugin(problems, repoFile, spec);
+
+                JSONObject vers = spec.getJSONObject("versions");
+                if (vers.getJSONObject("").isEmpty()) {
+                    merged.add(spec);
+                }
+
+                break; // FIXME
+            }
+        }
+
+        if (problems.size() > 0) {
+            throw new AssertionFailedError(problems.toString());
+        }
+
+        PrintWriter out = new PrintWriter(new File(repo.getAbsolutePath() + s + "all.json"));
+        out.print(merged.toString(1));
+        out.close();
+    }
+
+    private File[] getRepoFiles() throws IOException {
         String path = getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
         String up = File.separator + "..";
         String repos = path + up + up + up + File.separator + "site" + File.separator + "dat" + File.separator + "repo";
@@ -44,39 +77,48 @@ public class RepoTest {
         System.out.println("Working with " + dir.getCanonicalPath());
         File[] files = dir.listFiles();
         assert files != null;
-        for (File f : files) {
-            System.out.println("Checking repo: " + f.getCanonicalPath());
-            String content = new String(Files.readAllBytes(Paths.get(f.getAbsolutePath())), "UTF-8");
-            JSON json = JSONSerializer.toJSON(content, new JsonConfig());
-            JSONArray list = (JSONArray) json;
-            for (Object o : list) {
-                JSONObject spec = (JSONObject) o;
-                checkPlugin(problems, f, spec);
-            }
-        }
-
-        if (problems.size() > 0) {
-            throw new AssertionFailedError(problems.toString());
-        }
+        return files;
     }
 
-    private void checkPlugin(List<String> problems, File f, JSONObject spec) {
+    private void checkPlugin(List<String> problems, File repoFile, JSONObject spec) {
         Plugin plugin = Plugin.fromJSON(spec);
+        if (plugin.isVirtual()) {
+            return;
+        }
+
+        String maxVersion = plugin.getMaxVersion();
+        JSONObject maxVerObject = spec.getJSONObject("versions").getJSONObject(maxVersion);
+
+        JSONObject newVersions = new JSONObject();
+        newVersions.put(maxVersion, maxVerObject);
+
         try {
             System.out.println("Checking plugin: " + plugin);
-            plugin.setCandidateVersion(plugin.getMaxVersion());
+            plugin.setCandidateVersion(maxVersion);
             plugin.download(dummy);
 
             File jar = new File(plugin.getTempName());
             File dest = new File(plugin.getDestName());
             File to = new File(libExt.getAbsolutePath() + File.separator + dest.getName());
             jar.renameTo(to);
+            if (!maxVerObject.isEmpty()) {
+                maxVerObject.put("downloadUrl", "lib/ext/" + dest.getName());
+            }
         } catch (Throwable e) {
-            problems.add(f.getName() + ":" + plugin);
+            problems.add(repoFile.getName() + ":" + plugin);
             System.err.println("Problem with " + plugin);
             e.printStackTrace(System.err);
         }
 
+        checkLibs(problems, repoFile, plugin);
+
+        if (!maxVerObject.isEmpty()) {
+            newVersions.put(maxVersion, maxVerObject);
+            spec.put("versions", newVersions);
+        }
+    }
+
+    private void checkLibs(List<String> problems, File repoFile, Plugin plugin) {
         Map<String, String> libs = plugin.getLibs(plugin.getCandidateVersion());
         for (String id : libs.keySet()) {
             if (!cache.contains(libs.get(id))) {
@@ -90,13 +132,12 @@ public class RepoTest {
 
                     cache.add(libs.get(id));
                 } catch (Throwable e) {
-                    problems.add(f.getName() + ":" + plugin + ":" + id);
+                    problems.add(repoFile.getName() + ":" + plugin + ":" + id);
                     System.err.println("Problem with " + id);
                     e.printStackTrace(System.err);
                 }
             }
         }
-
     }
 
     private GenericCallback<String> dummy = new GenericCallback<String>() {

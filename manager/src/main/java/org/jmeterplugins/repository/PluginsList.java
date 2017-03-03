3a0fb862e1073d53452fa06ea618abcc72968a34
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-03-03 13:05:18 +0300

    make plugin list protected

--- manager/src/main/java/org/jmeterplugins/repository/PluginsList.java
+++ manager/src/main/java/org/jmeterplugins/repository/PluginsList.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -27,7 +28,7 @@ public class PluginsList extends JPanel implements ListSelectionListener, Hyperl
     private static final Logger log = LoggingManager.getLoggerForClass();
 
     private final JTextPane description = new JTextPane();
-    private JList<PluginCheckbox> list = new CheckBoxList<>(5);
+    protected JList<PluginCheckbox> list = new CheckBoxList<>(5);
     private DefaultListModel<PluginCheckbox> listModel = new DefaultListModel<>();
     protected final JComboBox<String> version = new JComboBox<>();
     private ItemListener itemListener = new VerChoiceChanged();

89e104073f0f5f688a84aa67c2fac00f6b1b5fa5
Andrey Pohilko <apc4@ya.ru>
2017-03-13 17:23:38 +0300

    improve bundle script

--- maven-bundle.sh
+++ maven-bundle.sh
@@ -1,3 +1,3 @@
 #! /bin/sh -xe
 
-mvn -Dmaven.test.skip=true -Dadditionalparam=-Xdoclint:none clean package javadoc:jar source:jar gpg:sign deploy
+mvn -Dmaven.test.skip=true -Dadditionalparam=-Xdoclint:none clean package javadoc:jar source:jar verify gpg:sign deploy

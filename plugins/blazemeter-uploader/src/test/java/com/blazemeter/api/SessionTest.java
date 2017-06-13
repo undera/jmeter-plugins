dde948d0d70c0fe9945ac4ba798cb37928f86445
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-06-13 17:09:10 +0300

    add session test

---
+++ plugins/blazemeter-uploader/src/test/java/com/blazemeter/api/SessionTest.java
@@ -0,0 +1,30 @@
+package com.blazemeter.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SessionTest {
+
+    @Test
+    public void test() throws Exception {
+        Session session = new Session();
+
+        String id = "id";
+        session.setId(id);
+        assertEquals(id, session.getId());
+
+        String userId = "userId";
+        session.setUserId(userId);
+        assertEquals(userId, session.getUserId());
+
+        String testId = "testId";
+        session.setTestId(testId);
+        assertEquals(testId, session.getTestId());
+
+        session = new Session(id, testId, userId);
+        assertEquals(id, session.getId());
+        assertEquals(userId, session.getUserId());
+        assertEquals(testId, session.getTestId());
+    }
+}

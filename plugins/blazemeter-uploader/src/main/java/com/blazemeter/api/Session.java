925a5411c0225317f72b09c9c0c27471e0c0f296
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-06-13 13:53:46 +0300

    add session entity

---
+++ plugins/blazemeter-uploader/src/main/java/com/blazemeter/api/Session.java
@@ -0,0 +1,41 @@
+package com.blazemeter.api;
+
+public class Session {
+
+    private String id;
+    private String testId;
+    private String userId;
+
+    public Session() {
+    }
+
+    public Session(String id, String testId, String userId) {
+        this.id = id;
+        this.testId = testId;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTestId() {
+        return testId;
+    }
+
+    public void setTestId(String testId) {
+        this.testId = testId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}

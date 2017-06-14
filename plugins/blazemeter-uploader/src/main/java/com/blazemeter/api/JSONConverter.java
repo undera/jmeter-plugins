0a148640a02d189b234ed3b8cdce07bc36c17c59
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-06-14 13:55:55 +0300

    add calculation Summary stats

--- plugins/blazemeter-uploader/src/main/java/com/blazemeter/api/JSONConverter.java
+++ plugins/blazemeter-uploader/src/main/java/com/blazemeter/api/JSONConverter.java
@@ -4,10 +4,13 @@ import net.sf.json.JSONObject;
 import org.apache.jmeter.samplers.SampleResult;
 import org.json.simple.JSONArray;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class JSONConverter {
 
@@ -64,37 +67,96 @@ public class JSONConverter {
         return res;
     }
 
-    /**
 
-     "first": self.owner.first_ts,
-     "last": self.owner.last_ts,
-     "duration": self.owner.last_ts - self.owner.first_ts,
-     "failed": cumul[KPISet.FAILURES],
-     "hits": cumul[KPISet.SAMPLE_COUNT],
+    private static JSONObject generateSummary(List<SampleResult> list) {
+        final JSONObject summaryValues = new JSONObject();
 
-     "avg": int(1000 * cumul[KPISet.AVG_RESP_TIME]),
-     "min": int(1000 * cumul[KPISet.PERCENTILES]["0.0"]) if "0.0" in cumul[KPISet.PERCENTILES] else 0,
-     "max": int(1000 * cumul[KPISet.PERCENTILES]["100.0"]) if "100.0" in cumul[KPISet.PERCENTILES] else 0,
-     "std": int(1000 * cumul[KPISet.STDEV_RESP_TIME]),
-     "tp90": int(1000 * cumul[KPISet.PERCENTILES]["90.0"]) if "90.0" in cumul[KPISet.PERCENTILES] else 0,
-     "tp95": int(1000 * cumul[KPISet.PERCENTILES]["95.0"]) if "95.0" in cumul[KPISet.PERCENTILES] else 0,
-     "tp99": int(1000 * cumul[KPISet.PERCENTILES]["99.0"]) if "99.0" in cumul[KPISet.PERCENTILES] else 0,
+        long first = Long.MAX_VALUE;
+        long last = 0;
+        long failed = 0;
+        long bytesCount = 0;
+        long sumTime = 0;
+        long sumLatency = 0;
+        Long[] rtimes = new Long[list.size()];
+        int counter = 0;
 
-     "latencyAvg": int(1000 * cumul[KPISet.AVG_LATENCY]),
-     "latencyMax": 0,
-     "latencyMin": 0,
-     "latencySTD": 0,
+        for (SampleResult sample : list) {
+            long endTime = sample.getEndTime();
 
-     "bytes": cumul[KPISet.BYTE_COUNT],
-     "bytesMax": 0,
-     "bytesMin": 0,
-     "bytesAvg": int(cumul[KPISet.BYTE_COUNT] / float(cumul[KPISet.SAMPLE_COUNT])),
-     "bytesSTD": 0,
+            if (endTime < first) {
+                first = endTime;
+            }
 
-     "otherErrorsSpillcount": 0,
-     */
-    private static Object generateSummary(List<SampleResult> list) {
-        return null;
+            if (endTime > last) {
+                last = endTime;
+            }
+
+            if (!sample.isSuccessful()) {
+                failed++;
+            }
+
+            bytesCount += sample.getBytes();
+            sumTime += sample.getTime();
+            sumLatency += sample.getLatency();
+            rtimes[counter] = sample.getTime();
+            counter++;
+        }
+
+        summaryValues.put("first", first);  // TODO: cast to sec?
+        summaryValues.put("last", last);     // TODO: cast to sec?
+        summaryValues.put("duration", last - first); // TODO: cast to sec?
+        summaryValues.put("failed", failed);
+        summaryValues.put("hits", list.size());
+
+        long average = sumTime / list.size();
+        summaryValues.put("avg", average);
+        Map<String, Long>  percentiles = getQuantiles(rtimes, average);
+        summaryValues.put("min", percentiles.containsKey("pect_0.0") ? percentiles.get("perc_0.0") : 0);
+        summaryValues.put("max", percentiles.containsKey("pect_100.0") ? percentiles.get("perc_100.0") : 0);
+        summaryValues.put("tp90", percentiles.containsKey("pect_90.0") ? percentiles.get("perc_90.0") : 0);
+        summaryValues.put("tp95", percentiles.containsKey("pect_95.0") ? percentiles.get("perc_95.0") : 0);
+        summaryValues.put("tp99", percentiles.containsKey("pect_99.0") ? percentiles.get("perc_99.0") : 0);
+        summaryValues.put("std", percentiles.containsKey("stdev") ? percentiles.get("stdev") : 0);
+
+        summaryValues.put("latencyAvg", sumLatency / list.size());
+        summaryValues.put("latencyMax", 0L);
+        summaryValues.put("latencyMin", 0L);
+        summaryValues.put("latencySTD", 0L);
+
+        summaryValues.put("bytes", bytesCount);
+        summaryValues.put("bytesMax", 0L);
+        summaryValues.put("bytesMin", 0L);
+        summaryValues.put("bytesAvg", bytesCount / list.size());
+        summaryValues.put("bytesSTD", 0L);
+
+        summaryValues.put("otherErrorsSpillcount", 0L);
+
+        return summaryValues;
+    }
+
+
+    public static Map<String, Long> getQuantiles(Long[] rtimes, long average) {
+        Map<String, Long> result = new JSONObject();
+        Arrays.sort(rtimes);
+
+        double[] quantiles = {0.0, 0.25, 0.50, 0.75, 0.80, 0.90, 0.95, 0.98, 0.99, 1.00};
+
+        Stack<Long> timings = new Stack<>();
+        timings.addAll(Arrays.asList(rtimes));
+        double level = 1.0;
+        Long timing = 0L;
+        long sqr_diffs = 0;
+        for (int qn = quantiles.length - 1; qn >= 0; qn--) {
+            double quan = quantiles[qn];
+            while (level >= quan && !timings.empty()) {
+                timing = timings.pop();
+                level -= 1.0 / rtimes.length;
+                sqr_diffs += timing * Math.pow(timing - average, 2);
+            }
+            result.put("perc_" + String.valueOf(quan), timing);
+        }
+        result.put("stdev", (long) Math.sqrt(sqr_diffs / rtimes.length));
+        return result;
     }
 
     private static Object generateAssertionsList(List<SampleResult> list) {

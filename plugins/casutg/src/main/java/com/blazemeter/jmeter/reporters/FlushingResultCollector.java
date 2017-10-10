42dd3337d905d12f44068cd45ff3164f5985d4de
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-10-10 16:23:42 +0300

    add hack for flush file (#183)

--- plugins/casutg/src/main/java/com/blazemeter/jmeter/reporters/FlushingResultCollector.java
+++ plugins/casutg/src/main/java/com/blazemeter/jmeter/reporters/FlushingResultCollector.java
@@ -1,8 +1,12 @@
 package com.blazemeter.jmeter.reporters;
 
 import org.apache.jmeter.reporters.ResultCollector;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 
 public class FlushingResultCollector extends ResultCollector {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
     public FlushingResultCollector() {
         super();
         getSaveConfig().setFieldNames(true);
@@ -11,6 +15,12 @@ public class FlushingResultCollector extends ResultCollector {
     @Override
     public void testEnded(String host) {
         super.testEnded(host);
-        flushFile();
+        try {
+            // hack for JMeter < 2.12 (BUG #56807)
+            ResultCollector.class.getDeclaredMethod("flushFile");
+            flushFile();
+        } catch (NoSuchMethodException e) {
+            log.warn("Cannot flush PrintWriter to file");
+        }
     }
 }

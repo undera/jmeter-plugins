c6b4c736a11739482d6a7630be2156271a193def
jpecinovsky <jpecinovsky@atlas.cz>
2017-11-04 08:18:40 +0100

    Headless option for Chrome web driver (#190)
    * Headless option for Chrome web driver
    
    * Headless option for Chrome web driver
    
    * Headless option for Chrome web driver

--- plugins/webdriver/src/main/java/com/googlecode/jmeter/plugins/webdriver/config/gui/ChromeDriverConfigGui.java
+++ plugins/webdriver/src/main/java/com/googlecode/jmeter/plugins/webdriver/config/gui/ChromeDriverConfigGui.java
@@ -13,6 +13,7 @@ public class ChromeDriverConfigGui extends WebDriverConfigGui {
     private static final long serialVersionUID = 100L;
     JTextField chromeServicePath;
     JCheckBox androidEnabled;
+    private JCheckBox headlessEnabled;
 
     @Override
     public String getStaticLabel() {
@@ -31,6 +32,7 @@ public class ChromeDriverConfigGui extends WebDriverConfigGui {
             ChromeDriverConfig config = (ChromeDriverConfig)element;
             chromeServicePath.setText(config.getChromeDriverPath());
             androidEnabled.setSelected(config.isAndroidEnabled());
+            getHeadlessEnabled().setSelected(config.isHeadlessEnabled());
         }
     }
 
@@ -48,6 +50,7 @@ public class ChromeDriverConfigGui extends WebDriverConfigGui {
             ChromeDriverConfig config = (ChromeDriverConfig)element;
             config.setChromeDriverPath(chromeServicePath.getText());
             config.setAndroidEnabled(androidEnabled.isSelected());
+            config.setHeadlessEnabled(getHeadlessEnabled().isSelected());
         }
     }
 
@@ -56,6 +59,7 @@ public class ChromeDriverConfigGui extends WebDriverConfigGui {
         super.clearGui();
         chromeServicePath.setText("");
         androidEnabled.setSelected(false);
+        getHeadlessEnabled().setSelected(false);
     }
 
     @Override
@@ -86,6 +90,8 @@ public class ChromeDriverConfigGui extends WebDriverConfigGui {
         androidEnabled = new JCheckBox("Use Chrome on Android");
         browserPanel.add(androidEnabled);
 
+        headlessEnabled = new JCheckBox("Use Chrome headless mode");
+        browserPanel.add(getHeadlessEnabled());
         return browserPanel;
     }
 
@@ -99,4 +105,7 @@ public class ChromeDriverConfigGui extends WebDriverConfigGui {
 		return true;
 	}
 
+    public JCheckBox getHeadlessEnabled() {
+        return headlessEnabled;
+    }
 }

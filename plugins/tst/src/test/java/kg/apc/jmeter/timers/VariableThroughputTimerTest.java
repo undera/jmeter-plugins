9924e1c88b02b037b0e8eace046ddbd0cb2f910f
pmouawad <p.mouawad@ubik-ingenierie.com>
2018-02-22 22:41:52 +0100

    Expose the duration of RPS which is sum of the last column Duration

--- plugins/tst/src/test/java/kg/apc/jmeter/timers/VariableThroughputTimerTest.java
+++ plugins/tst/src/test/java/kg/apc/jmeter/timers/VariableThroughputTimerTest.java
@@ -132,7 +132,7 @@ public class VariableThroughputTimerTest {
         long sec = 0L;
         VariableThroughputTimer instance = new VariableThroughputTimer();
         int expResult = -1;
-        double result = instance.getRPSForSecond(sec);
+        double result = instance.getRPSForSecond(sec).getLeft();
         assertEquals(expResult, result, 0.01);
     }
 

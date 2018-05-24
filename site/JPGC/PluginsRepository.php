7a574c8577d4d1b5593bfb3cad51470abff81e6c
Andrey Pohilko <apc4@ya.ru>
2018-05-24 16:52:00 +0300

    Return last-modified header for status report

--- site/JPGC/PluginsRepository.php
+++ site/JPGC/PluginsRepository.php
@@ -13,12 +13,32 @@ class PluginsRepository extends PWEModule implements Outputable
 {
     public function process()
     {
+        $plugins = $this->getRepoData(true);
         if ($_POST['stats'] && $_POST['stats'] != 'null') {
             PWELogger::warn("Status: %s", $_POST['stats']);
             throw new HTTP2xxException("", HTTP2xxException::OK);
         } else {
             PWELogger::warn("Status: [%s]", $_REQUEST['installID']);
         }
+
+
+        if ($_REQUEST['id']) {
+            foreach ($plugins as $plugin) {
+                if ($plugin['id'] == $_REQUEST['id']) {
+                    $plugins = array($plugin);
+                    break;
+                }
+            }
+        }
+
+        $max_age = 8 * 3600;
+        $this->PWE->sendHTTPHeader("Cache-Control: public, max-age=$max_age", true);
+        $this->PWE->sendHTTPHeader("Content-Type: application/json", true);
+        throw new HTTP2xxException(json_encode($plugins));
+    }
+
+    private function getRepoData($readFiles = false)
+    {
         $node = $this->PWE->getNode();
         $configsDir = $node['!a']['configs'];
         if (!$configsDir || !is_dir($configsDir)) {
@@ -33,24 +53,13 @@ class PluginsRepository extends PWEModule implements Outputable
             }
             $full_name = $configsDir . '/' . $fname;
             $oldest = max($oldest, filemtime($full_name));
-            $set = json_decode(file_get_contents($full_name), true);
-            $plugins = array_merge($plugins, $set);
-        }
-
-        if ($_REQUEST['id']) {
-            foreach ($plugins as $plugin) {
-                if ($plugin['id'] == $_REQUEST['id']) {
-                    $plugins = array($plugin);
-                    break;
-                }
+            if ($readFiles) {
+                $set = json_decode(file_get_contents($full_name), true);
+                $plugins = array_merge($plugins, $set);
             }
         }
 
-        $this->PWE->sendHTTPHeader('Last-Modified: ' . gmdate('D, d M Y H:i:s', $oldest) . ' GMT');
-        $max_age = 24 * 3600;
-        $this->PWE->sendHTTPHeader("Cache-Control: public, max-age=$max_age");
-
-        $this->PWE->sendHTTPHeader("Content-Type: application/json");
-        throw new HTTP2xxException(json_encode($plugins));
+        $this->PWE->sendHTTPHeader('Last-Modified: ' . gmdate('D, d M Y H:i:s', $oldest) . ' GMT', true);
+        return $plugins;
     }
 }

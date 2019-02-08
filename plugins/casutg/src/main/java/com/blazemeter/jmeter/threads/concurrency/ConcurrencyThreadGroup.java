5c52c069ccf6faa2ee23d0833005f361dde5f516
pmouawad <p.mouawad@ubik-ingenierie.com>
2019-02-08 16:52:36 +0100

    Fix CPU issue leading to 100% CPU during rampup: - Introduce temporisation when threads is empty to avoid CPU spinning - Replace synchronized/wait/notifyAll by Lock/Condition

--- plugins/casutg/src/main/java/com/blazemeter/jmeter/threads/concurrency/ConcurrencyThreadGroup.java
+++ plugins/casutg/src/main/java/com/blazemeter/jmeter/threads/concurrency/ConcurrencyThreadGroup.java
@@ -1,32 +1,51 @@
 package com.blazemeter.jmeter.threads.concurrency;
 
-import com.blazemeter.jmeter.threads.AbstractDynamicThreadGroup;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.threads.JMeterThread;
 import org.apache.jmeter.threads.ListenerNotifier;
+import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.ListedHashTree;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
+import com.blazemeter.jmeter.threads.AbstractDynamicThreadGroup;
+
 public class ConcurrencyThreadGroup extends AbstractDynamicThreadGroup {
     private static final Logger log = LoggingManager.getLoggerForClass();
-    public static final int MIN_CHECK_TIME = 1000;
+    private static final long DEFAULT_TEMPORISATION = JMeterUtils.getPropDefault("dynamic_tg.temporisation", 10L);
 
+    public static final long MIN_CHECK_TIME = 1000L;
+    private final transient Lock lock = new ReentrantLock();
+    private final transient Condition condition = lock.newCondition(); 
+    
     @Override
     protected Thread getThreadStarter(int groupIndex, ListenerNotifier listenerNotifier, ListedHashTree testTree, StandardJMeterEngine engine) {
         return new ConcurrencyThreadStarter(groupIndex, listenerNotifier, testTree, engine, this);
     }
 
-    public synchronized void waitThreadStopped() { // FIXME: method named in confusing way
-        if (!threads.isEmpty()) {
-            try {
-                wait(MIN_CHECK_TIME);
-            } catch (InterruptedException e) {
-                log.debug("Interrupted", e);
-            }
+    /**
+     * If threads is empty we wait on condition for 1s max to be notified,
+     *  otherwise 
+     */
+    public void waitThreadStopped() { // FIXME: method named in confusing way
+        long sleepTime = threads.isEmpty() ? DEFAULT_TEMPORISATION : MIN_CHECK_TIME;
+        lock.lock();
+        try {
+            condition.await(sleepTime, TimeUnit.MILLISECONDS); 
+        } catch (InterruptedException e) {
+            log.debug("Interrupted", e);
+            Thread.currentThread().interrupt();
+        } finally {
+            lock.unlock();
         }
     }
 
+    @Override
     public int getNumThreads() {
         return (int) Math.round(getTargetLevelAsDouble());
     }
@@ -40,12 +59,20 @@ public class ConcurrencyThreadGroup extends AbstractDynamicThreadGroup {
         saveLogRecord("START", thread.getThreadName(), "");
     }
 
+    public ConcurrencyThreadGroup() {
+        super();
+        
+    }
+
     @Override
     public void threadFinished(JMeterThread thread) {
         super.threadFinished(thread);
         saveLogRecord("FINISH", thread.getThreadName(), "");
-        synchronized (this) {
-            notifyAll();
+        lock.lock();
+        try {
+            condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 

dc36b92e289fb87630733231a118215efaf5ae7b
pmouawad <p.mouawad@ubik-ingenierie.com>
2019-02-08 16:54:17 +0100

    Cache properties as they are read at high rate leading to high memory pressure. Surround logging with isDebugEnabled since getPlannedConcurrency() has a high cost Round maxConcurr on construction instead of doing it every time

--- plugins/casutg/src/main/java/com/blazemeter/jmeter/threads/concurrency/ConcurrencyThreadStarter.java
+++ plugins/casutg/src/main/java/com/blazemeter/jmeter/threads/concurrency/ConcurrencyThreadStarter.java
@@ -1,7 +1,5 @@
 package com.blazemeter.jmeter.threads.concurrency;
 
-import com.blazemeter.jmeter.threads.AbstractThreadStarter;
-import com.blazemeter.jmeter.threads.DynamicThread;
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.threads.ListenerNotifier;
 import org.apache.jmeter.util.JMeterUtils;
@@ -9,23 +7,40 @@ import org.apache.jorphan.collections.ListedHashTree;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
+import com.blazemeter.jmeter.threads.AbstractThreadStarter;
+import com.blazemeter.jmeter.threads.DynamicThread;
+
 public class ConcurrencyThreadStarter extends AbstractThreadStarter {
     private static final Logger log = LoggingManager.getLoggerForClass();
+    private static final long DEFAULT_SHIFT_RAMPUP = JMeterUtils.getPropDefault("dynamic_tg.shift_rampup_start", 0L);
 
     private final ConcurrencyThreadGroup concurrTG;
+    // We cache values for performance
+    private long rampUp;
+    private long hold;
+    private long steps;
+    private long maxConcurr;
 
     public ConcurrencyThreadStarter(int groupIndex, ListenerNotifier listenerNotifier, ListedHashTree testTree, StandardJMeterEngine engine, ConcurrencyThreadGroup concurrencyThreadGroup) {
         super(groupIndex, concurrencyThreadGroup, testTree, listenerNotifier, engine);
         concurrTG = concurrencyThreadGroup;
+        // We cache values
+        this.rampUp = owner.getRampUpSeconds();
+        this.hold = owner.getHoldSeconds();
+        this.steps = owner.getStepsAsLong();
+        this.maxConcurr = Math.round(owner.getTargetLevelAsDouble());
     }
 
     @Override
     protected void supplyActiveThreads() throws InterruptedException {
         log.info("Start supplying threads");
         startTime = System.currentTimeMillis();
-        while (!owner.isLimitReached() && getPlannedConcurrency() >= 0) {
-            log.debug("Concurrency factual/expected: " + concurrTG.getConcurrency() + "/" + getPlannedConcurrency());
-            while (concurrTG.getConcurrency() < getPlannedConcurrency()) {
+        boolean isDebugEnabled = log.isDebugEnabled();
+        while (!owner.isLimitReached() && getPlannedConcurrency(isDebugEnabled) >= 0) {
+            if(isDebugEnabled) {
+                log.debug("Concurrency factual/expected: " + concurrTG.getConcurrency() + "/" + getPlannedConcurrency(isDebugEnabled));
+            }
+            while (concurrTG.getConcurrency() < getPlannedConcurrency(isDebugEnabled)) {
                 DynamicThread thread = addActiveThread();
                 concurrTG.threadStarted(thread);
             }
@@ -34,16 +49,13 @@ public class ConcurrencyThreadStarter extends AbstractThreadStarter {
         log.info("Done supplying threads");
     }
 
-    private long getPlannedConcurrency() {
-        long rampUp = owner.getRampUpSeconds();
-        long hold = owner.getHoldSeconds();
-        long steps = owner.getStepsAsLong();
-        double maxConcurr = owner.getTargetLevelAsDouble();
+    private long getPlannedConcurrency(boolean isDebugEnabled) {
         double timeOffset = (System.currentTimeMillis() - startTime) / 1000.0;
-        log.debug("Time progress: " + timeOffset + "/" + (rampUp + hold));
+        if(isDebugEnabled) {
+            log.debug("Time progress: " + timeOffset + "/" + (rampUp + hold));
+        }
 
-        long shift = JMeterUtils.getPropDefault("dynamic_tg.shift_rampup_start", 0L);
-        timeOffset -= shift;
+        timeOffset -= DEFAULT_SHIFT_RAMPUP;
         if (timeOffset < 0) {
             timeOffset = 0;
         }
@@ -53,13 +65,13 @@ public class ConcurrencyThreadStarter extends AbstractThreadStarter {
         }
 
         if (rampUp == 0 || timeOffset > rampUp) {
-            return Math.round(maxConcurr);
+            return maxConcurr;
         } else if (steps > 0) {
             double stepSize = maxConcurr / (double) steps;
             double stepLen = rampUp / (double) steps;
             return Math.round(stepSize * (Math.floor(timeOffset / stepLen) + 1));
         } else {
-            double slope = maxConcurr / rampUp;
+            double slope = (double) maxConcurr / rampUp;
             return Math.round(slope * timeOffset);
         }
     }

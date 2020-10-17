7f28ba42f8a9fed0b82199941ef0d0da87e32689
Andrey Pokhilko <apc4@ya.ru>
2020-10-17 21:33:55 +0300

    fail on wrong upload

--- site-upload.sh
+++ site-upload.sh
@@ -5,5 +5,5 @@ echo TRAVIS_BRANCH: $TRAVIS_BRANCH
 echo TRAVIS_EVENT_TYPE: $TRAVIS_EVENT_TYPE
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_EVENT_TYPE" != 'pull_request' ]; then
-  curl -vfk https://jmeter-plugins.org/unzip.php?job=$TRAVIS_JOB_ID -F "zipfile=@upload/site.zip" -H "Authorization: Bearer $UPLOAD_TOKEN"
+  curl --fail -vfk https://jmeter-plugins.org/unzip.php?job=$TRAVIS_JOB_ID -F "zipfile=@upload/site.zip" -H "Authorization: Bearer $UPLOAD_TOKEN"
 fi

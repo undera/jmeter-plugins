84876b1ef5b08c85519c27090d3b0da68b5c08ed
Andrey Pokhilko <apc4@ya.ru>
2020-10-17 20:51:56 +0300

    Switch upload

--- site-upload.sh
+++ site-upload.sh
@@ -1,6 +1,5 @@
 #! /bin/sh -ex
 
-tar -xzf upload.tgz
-
-# upload
-ncftpput -f ~/.jmeter-plugins.org-ncftp.cfg -R -F / upload/*
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_EVENT_TYPE" != 'pull_request' ]; then
+  curl -vfk https://jmeter-plugins.org/unzip.php -F "zipfile=@upload/site.zip" -H "Authorization: Bearer $UPLOAD_TOKEN"
+fi

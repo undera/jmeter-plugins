41a892ed3510daf7225d1f798752921f973b7b7e
Andrey Pokhilko <apc4@ya.ru>
2020-10-17 21:16:46 +0300

    Experiments

--- site-upload.sh
+++ site-upload.sh
@@ -1,5 +1,5 @@
 #! /bin/sh -ex
 
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_EVENT_TYPE" != 'pull_request' ]; then
-  curl -vfk https://jmeter-plugins.org/unzip.php -F "zipfile=@upload/site.zip" -H "Authorization: Bearer $UPLOAD_TOKEN"
+  curl -vfk https://jmeter-plugins.org/unzip.php?job=$TRAVIS_JOB_ID -F "zipfile=@upload/site.zip" -H "Authorization: Bearer $UPLOAD_TOKEN"
 fi

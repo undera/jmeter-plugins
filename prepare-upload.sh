378c028ce514190ed8474180b2e08033108ade7d
Andrey Pokhilko <apc4@ya.ru>
2023-02-16 15:00:30 +0000

    Switch from TravisCI onto GH Actions (#570)
    * Trying to fix
    
    * Test
    
    * More tries
    
    * More
    
    * Step
    
    * Try to find
    
    * Tell me the OS
    
    * Diagnose
    
    * no dpkg
    
    * TEST GH action
    
    * Experimenting with scripts
    
    * Package
    
    * run packaging
    
    * Remove nightly snapshots
    
    * Optimize
    
    * More test
    
    * Use secret
    
    * Different PHP ver
    
    * Come on, come on
    
    * Test it would publish
    
    * Re-enable things
    
    * Add bit of log
    
    * Copy step in correct place

--- prepare-upload.sh
+++ prepare-upload.sh
@@ -1,33 +1,12 @@
 #! /bin/sh -ex
 
-REV=`git log -1 --format="%H" | cut -c1-10`
-
-# build
-#mvn clean cobertura:cobertura package
+python downloads.py
 
 rm -rf upload
 mkdir -p upload
 
 # site docs
 cp -r site/* upload/
-
-# package snapshots
-mkdir -p upload/files/nightly
-
-python downloads.py
-mkdir -p upload/files/packages
-mv site/files/packages/* upload/files/packages/
-
-for D in `ls` ; do
-    if ls $D/target/*-*.zip 2>/dev/null ; then
-        cp $D/target/*-*.zip upload/files/nightly/
-    fi
-done
-
-#cp manager/target/jmeter-plugins-manager-*.jar upload/files/nightly/
-
-rename "s/.jar/_$REV.jar/" upload/files/nightly/*.jar
-rename "s/.zip/_$REV.zip/" upload/files/nightly/*.zip
 
 # examples
 cp -r examples upload/img/

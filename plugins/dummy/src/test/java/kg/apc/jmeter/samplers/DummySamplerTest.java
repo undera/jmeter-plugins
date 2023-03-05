0636ece37d7adfe869d1762fcf640bd86050972b
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-03-05 09:37:34 +0000

    Force encoding

--- plugins/dummy/src/test/java/kg/apc/jmeter/samplers/DummySamplerTest.java
+++ plugins/dummy/src/test/java/kg/apc/jmeter/samplers/DummySamplerTest.java
@@ -62,6 +62,7 @@ public class DummySamplerTest {
         DummySampler instance = new DummySampler();
         instance.getDummy().setResponseData(data);
         SampleResult result = instance.sample(null);
+        result.setDataEncoding("UTF-8");
         System.out.println("Enc: " + result.getDataEncodingWithDefault());
         Assert.assertNotNull(result);
         // freaking "static final" DEFAULT_ENCODING field in SampleResult does not allow us to assert this

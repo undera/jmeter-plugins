74546f75a6a5863b283f451f6053933f7a546aa3
Andrei Pohilko <andrei.pokhilko@gmail.com>
2026-02-15 09:45:48 +0000

    Bump up to Java 11

--- infra/common/src/test/java/kg/apc/charting/colors/CustomPaletteTest.java
+++ infra/common/src/test/java/kg/apc/charting/colors/CustomPaletteTest.java
@@ -3,14 +3,10 @@ package kg.apc.charting.colors;
 import kg.apc.charting.ColorsDispatcher;
 import kg.apc.charting.ColorsDispatcherFactory;
 import org.apache.jmeter.util.JMeterUtils;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
@@ -18,131 +14,92 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.validateMockitoUsage;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({JMeterUtils.class})
 public class CustomPaletteTest {
-    @After
-    public void validate() {
-        validateMockitoUsage();
-    }
 
-    /**
-     * Test factory retrieves CustomPalette with null options
-     */
     @Test
     public void testFactoryNullPaletteNull() {
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        // PowerMockito.verifyStatic(JMeterUtils.class, Mockito.times(1)); // FIXME: what powemock wants from me?
-        assertEquals("CustomPalette", instance.getClass().getSimpleName());
-    }
-
-    /**
-     * Test factory retrieves CustomPalette with empty options
-     */
-    @Test
-    public void testFactoryEmptyPaletteEmpty() {
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn("");
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        // FIXME: PowerMockito.verifyStatic(JMeterUtils.class);
-        assertEquals("CustomPalette", instance.getClass().getSimpleName());
-    }
-
-    /**
-     * Test factory retrieves CustomPalette with partial options
-     */
-    @Test
-    public void testFactoryEmptyPaletteSingle() {
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher"))
-                .thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options"))
-                .thenReturn("9C27B0");
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        // FIXME: PowerMockito.verifyStatic(JMeterUtils.class);
-        assertEquals("CustomPalette", instance.getClass().getSimpleName());
-    }
-
-    /**
-     * Test factory retrieves CustomPalette with partial options
-     */
-    @Test
-    public void testFactoryEmptyPaletteMany() {
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher"))
-                .thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options"))
-                .thenReturn("9C27B0,9C27B0,9C27B0,9C27B0,9C27B0,9C27B0,9C27B1,9C27B2,9C27B3,9C27B4,9C27B5,9C27B6,9C27B7,9C27B8,9C27B9,9C27BA,9C27BB,9C27BC,9C27BD,9C27BD,9C27BE,9C27BF");
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        // FIXME: PowerMockito.verifyStatic(JMeterUtils.class);
-        assertEquals("CustomPalette", instance.getClass().getSimpleName());
-    }
-
-    /**
-     * Test factory retrieves CustomPalette with partial options
-     */
-    @Test
-    public void testFactoryEmptyPaletteInvalidColors() {
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher"))
-                .thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options"))
-                .thenReturn("9C27B0,9C27B0,9C27B0,9Ca27B0,9C27B0,9C27B0,9C2f7B1,9C27B2,9C2f7B3,9C27B4,9C27B5,9C27B6,9C27B7,9C27B8,9C27B9,9C27BA,9C27BB,9C27BC,9C27BD,9C27BD,9C27BE,9C27BF");
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        // FIXME: PowerMockito.verifyStatic(JMeterUtils.class);
-        assertEquals("CustomPalette", instance.getClass().getSimpleName());
-    }
-
-    /**
-     * Test of getNextColor method, of class ColorsDispatcher.
-     */
-    @Test
-    public void testGetNextColorNull() {
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        // FIXME: PowerMockito.verifyStatic(JMeterUtils.class);
-        for (int n = 0; n < 2000; n++) {
-            Color c = instance.getNextColor();
-            System.out.println(c);
-            Assert.assertNotNull(c);
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            assertEquals("CustomPalette", instance.getClass().getSimpleName());
         }
     }
 
-    /**
-     * Test of reset method, of class ColorsDispatcher.
-     */
     @Test
-    public void testReset() {
-        System.out.println("reset");
-        PowerMockito.mockStatic(JMeterUtils.class);
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
-        PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
-        ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
-        Color first = instance.getNextColor();
-        Assert.assertNotNull(first);
-        instance.getNextColor();
-        instance.getNextColor();
-        instance.reset();
-        assertEquals(first, instance.getNextColor());
+    public void testFactoryEmptyPaletteEmpty() {
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn("");
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            assertEquals("CustomPalette", instance.getClass().getSimpleName());
+        }
     }
 
-    /**
-     * Test that the object is serializable
-     */
+    @Test
+    public void testFactoryEmptyPaletteSingle() {
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn("9C27B0");
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            assertEquals("CustomPalette", instance.getClass().getSimpleName());
+        }
+    }
+
+    @Test
+    public void testFactoryEmptyPaletteMany() {
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn("9C27B0,9C27B0,9C27B0,9C27B0,9C27B0,9C27B0,9C27B1,9C27B2,9C27B3,9C27B4,9C27B5,9C27B6,9C27B7,9C27B8,9C27B9,9C27BA,9C27BB,9C27BC,9C27BD,9C27BD,9C27BE,9C27BF");
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            assertEquals("CustomPalette", instance.getClass().getSimpleName());
+        }
+    }
+
+    @Test
+    public void testFactoryEmptyPaletteInvalidColors() {
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn("9C27B0,9C27B0,9C27B0,9Ca27B0,9C27B0,9C27B0,9C2f7B1,9C27B2,9C2f7B3,9C27B4,9C27B5,9C27B6,9C27B7,9C27B8,9C27B9,9C27BA,9C27BB,9C27BC,9C27BD,9C27BD,9C27BE,9C27BF");
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            assertEquals("CustomPalette", instance.getClass().getSimpleName());
+        }
+    }
+
+    @Test
+    public void testGetNextColorNull() {
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            for (int n = 0; n < 2000; n++) {
+                Color c = instance.getNextColor();
+                Assert.assertNotNull(c);
+            }
+        }
+    }
+
+    @Test
+    public void testReset() {
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
+            ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
+            Color first = instance.getNextColor();
+            Assert.assertNotNull(first);
+            instance.getNextColor();
+            instance.getNextColor();
+            instance.reset();
+            assertEquals(first, instance.getNextColor());
+        }
+    }
+
     @Test
     public void testSerialization() {
-        try {
-            PowerMockito.mockStatic(JMeterUtils.class);
-            PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
-            PowerMockito.when(JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
+        try (MockedStatic<JMeterUtils> mocked = Mockito.mockStatic(JMeterUtils.class)) {
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher")).thenReturn("custompalette");
+            mocked.when(() -> JMeterUtils.getProperty("jmeterPlugin.customColorsDispatcher.options")).thenReturn(null);
             ColorsDispatcher instance = ColorsDispatcherFactory.getColorsDispatcher();
             new ObjectOutputStream(new ByteArrayOutputStream()).writeObject(instance);
             Assert.assertTrue(true);
@@ -150,5 +107,4 @@ public class CustomPaletteTest {
             Assert.fail(e.getClass().getName() + ": " + e.getMessage());
         }
     }
-
 }
